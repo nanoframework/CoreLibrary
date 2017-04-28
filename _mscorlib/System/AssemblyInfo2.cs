fa5f6c85a08a9a4740dccb122556d6f1c5835000
José Simões <josesimoes@users.noreply.github.com>
2017-04-28 17:58:27 +0100

    Several improvements (#2)
    - update MSBuildSystem Nuget package
    - assembly is now signed
    - add Nuget package for testing and deployment to nanoCLR solutions
    - bumped Nuget package to preview005
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- _mscorlib/System/AssemblyInfo2.cs
+++ _mscorlib/System/AssemblyInfo2.cs
@@ -3,3 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("4.0.0.0")]
+
+[assembly: AssemblyFileVersion("4.0.0.0")]
