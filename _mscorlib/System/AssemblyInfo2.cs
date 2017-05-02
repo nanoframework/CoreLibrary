ebd0378941d46f184a7287cd2935f8d04f1aeed3
José Simões <josesimoes@users.noreply.github.com>
2017-05-02 19:47:58 +0100

    Improvements in Core Library solution (#5)
    - rename project for clarity
    - add new properties to match latest updates in MSBuild System targets regarding core libraries build
    - rename TEST Nuget project to DELIVERABLES for clarity
    - update DELIVERABLES sumary and description for clarity
    - bumped Nuget versions to preview007
    - update mscorlib version to 1.0.0
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- _mscorlib/System/AssemblyInfo2.cs
+++ _mscorlib/System/AssemblyInfo2.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
