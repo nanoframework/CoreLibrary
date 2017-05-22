d7ec05148bfae9f0752ca5811a8a37138a5deadc
Christophe Gerbier <christophe@mikrobusnet.org>
2017-05-23 01:27:58 +0200

    Issue #39 - System.Diagnostics (#66)
    Comments review on System.Diagnostics
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

--- CoreLibrary/System/Diagnostics/ConditionalAttribute.cs
+++ CoreLibrary/System/Diagnostics/ConditionalAttribute.cs
@@ -4,27 +4,38 @@
 // See LICENSE file in the project root for full license information.
 //
 
-using System;
-
 namespace System.Diagnostics
 {
+    /// <summary>
+    /// Indicates to compilers that a method call or attribute should be ignored unless a specified conditional compilation symbol is defined.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true), Serializable]
     public sealed class ConditionalAttribute : Attribute
     {
+        private readonly String _conditionString;
+
+        /// <summary>
+        /// Initializes a new instance of the ConditionalAttribute class.
+        /// </summary>
+        /// <param name="conditionString">A string that specifies the case-sensitive conditional compilation symbol that is associated with the attribute.</param>
         public ConditionalAttribute(String conditionString)
         {
-            m_conditionString = conditionString;
+            _conditionString = conditionString;
         }
 
+        /// <summary>
+        /// Gets the conditional compilation symbol that is associated with the ConditionalAttribute attribute.
+        /// </summary>
+        /// <value>A string that specifies the case-sensitive conditional compilation symbol that is associated with the ConditionalAttribute attribute.</value>
         public String ConditionString
         {
             get
             {
-                return m_conditionString;
+                return _conditionString;
             }
         }
 
-        private String m_conditionString;
+        
     }
 }
 
