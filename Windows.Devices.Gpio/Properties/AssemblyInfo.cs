e37f1534911ec1bf35a632d0bdaa4cba4e77579a
José Simões <josesimoes@users.noreply.github.com>
2017-06-28 02:23:25 +0100

    Rename projects (#93)
    - rename projects to the new extension
    - rework projects as required
    - update Nuget packages
    - bump Nuget packages
    - update AppVeyor to install nanoFramework extension 
    
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- Windows.Devices.Gpio/Properties/AssemblyInfo.cs
+++ Windows.Devices.Gpio/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -8,6 +10,11 @@
 [assembly: AssemblyProduct("Windows.Devices.Gpio")]
 [assembly: AssemblyCopyright("Copyright © nanoFrameowrk Contributors 2017")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -15,5 +22,8 @@
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
