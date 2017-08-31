4737db5264761adaf0d63e6ba8cc136f4cb09f75
Christophe Gerbier <christophe@mikrobusnet.org>
2017-08-31 19:40:39 +0200

    Work on System.Convert (#144)
    Use of NativeToInt64() and NativeToDouble() instead of the managed methods.
    
    Signed-off-by: Christophe Gerbier <christophe@mikrobusnet.org>

--- CoreLibrary/System/Convert.cs
+++ CoreLibrary/System/Convert.cs
@@ -31,6 +31,12 @@ namespace System
     [ComponentModel.EditorBrowsableAttribute(ComponentModel.EditorBrowsableState.Never)]
     public static class Convert
     {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern long NativeToInt64(string value, bool signed, long min, long max, int radix);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern double NativeToDouble(string value);
+
         /// <summary>
         /// Converts the value of the specified 16-bit unsigned integer to its equivalent Unicode character.
         /// </summary>
@@ -48,9 +54,9 @@ namespace System
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>An 8-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
         [CLSCompliant(false)]
-        public static sbyte ToSByte(string value)
+        public static sbyte ToSByte(string value, int radix = 10)
         {
-            return (sbyte)ToInt64(value, true, SByte.MinValue, SByte.MaxValue);
+            return (sbyte)NativeToInt64(value, true, SByte.MinValue, SByte.MaxValue, radix);
         }
 
         /// <summary>
@@ -58,9 +64,9 @@ namespace System
         /// </summary>
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>An 8-bit unsigned integer that is equivalent to value, or zero if value is null.</returns>
-        public static byte ToByte(string value)
+        public static byte ToByte(string value, int radix = 10)
         {
-            return (byte)ToInt64(value, false, Byte.MinValue, Byte.MaxValue);
+            return (byte)NativeToInt64(value, false, Byte.MinValue, Byte.MaxValue, radix);
         }
 
         /// <summary>
@@ -68,9 +74,9 @@ namespace System
         /// </summary>
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>A 16-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
-        public static short ToInt16(string value)
+        public static short ToInt16(string value, int radix = 10)
         {
-            return (short)ToInt64(value, true, Int16.MinValue, Int16.MaxValue);
+            return (short)NativeToInt64(value, true, Int16.MinValue, Int16.MaxValue, radix);
         }
 
         /// <summary>
@@ -79,9 +85,9 @@ namespace System
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>A 16-bit unsigned integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
         [CLSCompliant(false)]
-        public static ushort ToUInt16(string value)
+        public static ushort ToUInt16(string value, int radix = 10)
         {
-            return (ushort)ToInt64(value, false, UInt16.MinValue, UInt16.MaxValue);
+            return (ushort)NativeToInt64(value, false, UInt16.MinValue, UInt16.MaxValue, radix);
         }
 
         /// <summary>
@@ -89,9 +95,9 @@ namespace System
         /// </summary>
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>A 32-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
-        public static int ToInt32(string value)
+        public static int ToInt32(string value, int radix = 10)
         {
-            return (int)ToInt64(value, true, Int32.MinValue, Int32.MaxValue);
+            return (int)NativeToInt64(value, true, Int32.MinValue, Int32.MaxValue, radix);
         }
 
         /// <summary>
@@ -100,9 +106,9 @@ namespace System
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>A 32-bit unsigned integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
         [CLSCompliant(false)]
-        public static uint ToUInt32(string value)
+        public static uint ToUInt32(string value, int radix = 10)
         {
-            return (uint)ToInt64(value, false, UInt32.MinValue, UInt32.MaxValue);
+            return (uint)NativeToInt64(value, false, UInt32.MinValue, UInt32.MaxValue, radix);
         }
 
         /// <summary>
@@ -110,9 +116,9 @@ namespace System
         /// </summary>
         /// <param name="value">A string that contains a number to convert.</param>
         /// <returns>A 64-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
-        public static long ToInt64(string value)
+        public static long ToInt64(string value, int radix = 10)
         {
-            return ToInt64(value, true, Int64.MinValue, Int64.MaxValue);
+            return NativeToInt64(value, true, Int64.MinValue, Int64.MaxValue, radix);
         }
 
         /// <summary>
@@ -121,98 +127,9 @@ namespace System
         /// <param name="value">A string that contains the number to convert.</param>
         /// <returns>A 64-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
         [CLSCompliant(false)]
-        public static ulong ToUInt64(string value)
+        public static ulong ToUInt64(string value, int radix = 10)
         {
-            return (ulong)ToInt64(value, false, 0, 0);
-        }
-
-        /// <summary>
-        /// Converts the string representation of a number in a specified base to an equivalent 32-bit signed integer.
-        /// </summary>
-        /// <param name="hexNumber">A string that contains the number to convert.</param>
-        /// <param name="fromBase">The base of the number in value, which must be 2, 8, 10, or 16.</param>
-        /// <returns>A 32-bit signed integer that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
-        /// <exception cref="ArgumentException"></exception>
-        public static int ToInt32(string hexNumber, int fromBase)
-        {
-            if (hexNumber == null) return 0;
-            if (fromBase != 16) throw new ArgumentException();
-
-            var result = 0;
-            var hexDigit = hexNumber.Trim(' ').ToUpper().ToCharArray();
-
-            // Trim hex sentinal if present 
-            var len = hexDigit.Length;
-            var i = len >= 2 && hexDigit[0] == '0' && hexDigit[1] == 'X' ? 2 : 0;
-
-            // 8 hex chars == 4 bytes == sizeof(Int32)
-            if (len - i > 8) throw new ArgumentException();
-
-            // Convert hex to integer
-            for (; i < len; i++)
-            {
-                var c = hexDigit[i];
-
-                int digit;
-                switch (c)
-                {
-                    case '0':
-                    digit = 0;
-                    break;
-                    case '1':
-                    digit = 1;
-                    break;
-                    case '2':
-                    digit = 2;
-                    break;
-                    case '3':
-                    digit = 3;
-                    break;
-                    case '4':
-                    digit = 4;
-                    break;
-                    case '5':
-                    digit = 5;
-                    break;
-                    case '6':
-                    digit = 6;
-                    break;
-                    case '7':
-                    digit = 7;
-                    break;
-                    case '8':
-                    digit = 8;
-                    break;
-                    case '9':
-                    digit = 9;
-                    break;
-                    case 'A':
-                    digit = 10;
-                    break;
-                    case 'B':
-                    digit = 11;
-                    break;
-                    case 'C':
-                    digit = 12;
-                    break;
-                    case 'D':
-                    digit = 13;
-                    break;
-                    case 'E':
-                    digit = 14;
-                    break;
-                    case 'F':
-                    digit = 15;
-                    break;
-                    default:
-                    throw new ArgumentException();
-                }
-
-                result <<= 4;
-                result += digit;
-            }
-
-            return result;
+            return (ulong)NativeToInt64(value, true, 0, 0, radix);
         }
 
         /// <summary>
@@ -222,316 +139,7 @@ namespace System
         /// <returns>A double-precision floating-point number that is equivalent to the number in value, or 0 (zero) if value is null.</returns>
         public static double ToDouble(string s)
         {
-            if (s == null) return 0;
-
-            s = s.Trim(' ').ToLower();
-
-            if (s.Length == 0) return 0;
-
-            var decimalpoint = s.IndexOf('.');
-            var exp = s.IndexOf('e');
-
-            if (exp != -1 && decimalpoint > exp) throw new Exception();
-
-            var chars = s.ToCharArray();
-            var len = chars.Length;
-            double power = 0;
-            double rightDecimal = 0;
-            var decLeadingZeros = 0;
-            double leftDecimal = 0;
-            var leftDecLen = 0;
-            var isNeg = chars[0] == '-';
-
-            // convert the exponential portion to a number            
-            if (exp != -1 && exp + 1 < len - 1)
-            {
-                int tmp;
-                power = GetDoubleNumber(chars, exp + 1, len - (exp + 1), out tmp);
-            }
-
-            // convert the decimal portion to a number
-            if (decimalpoint != -1)
-            {
-                int decLen;
-
-                if (exp == -1) decLen = len - (decimalpoint + 1);
-                else decLen = exp - (decimalpoint + 1);
-
-                var number = GetDoubleNumber(chars, decimalpoint + 1, decLen, out decLeadingZeros);
-
-                rightDecimal = number * Math.Pow(10, -decLen);
-            }
-
-            // convert the integer portion to a number
-            if (decimalpoint != 0)
-            {
-                int leadingZeros;
-
-                if (decimalpoint == -1 && exp == -1) leftDecLen = len;
-                else if (decimalpoint != -1) leftDecLen = decimalpoint;
-                else leftDecLen = exp;
-
-                leftDecimal = GetDoubleNumber(chars, 0, leftDecLen, out leadingZeros);
-                // subtract leading zeros from integer length
-                leftDecLen -= leadingZeros;
-
-                if (chars[0] == '-' || chars[0] == '+') leftDecLen--;
-            }
-
-            double value;
-            if (leftDecimal < 0)
-            {
-                value = -leftDecimal + rightDecimal;
-                value = -value;
-            }
-            else value = leftDecimal + rightDecimal;
-
-            // lets normalize the integer portion first
-            while (leftDecLen > 1)
-            {
-                switch (leftDecLen)
-                {
-                    case 2:
-                    value /= 10.0;
-                    power += 1;
-                    leftDecLen -= 1;
-                    break;
-                    case 3:
-                    value /= 100.0;
-                    power += 2;
-                    leftDecLen -= 2;
-                    break;
-                    case 4:
-                    value /= 1000.0;
-                    power += 3;
-                    leftDecLen -= 3;
-                    break;
-                    default:
-                    value /= 10000.0;
-                    power += 4;
-                    leftDecLen -= 4;
-                    break;
-                }
-            }
-
-            // now normalize the decimal portion
-            if (value != 0.0 && value < 1.0 && value > -1.0)
-            {
-                // for normalization we want x.xxx instead of 0.xxx
-                decLeadingZeros++;
-
-                while (decLeadingZeros > 0)
-                {
-                    switch (decLeadingZeros)
-                    {
-                        case 1:
-                        value *= 10.0;
-                        power -= 1;
-                        decLeadingZeros -= 1;
-                        break;
-                        case 2:
-                        value *= 100.0;
-                        power -= 2;
-                        decLeadingZeros -= 2;
-                        break;
-                        case 3:
-                        value *= 1000.0;
-                        power -= 3;
-                        decLeadingZeros -= 3;
-                        break;
-                        default:
-                        value *= 10000.0;
-                        power -= 4;
-                        decLeadingZeros -= 4;
-                        break;
-                    }
-                }
-            }
-
-            // special case for epsilon (the System.Math.Pow native method will return zero for -324)
-            if (power == -324)
-            {
-                value = value * Math.Pow(10, power + 1);
-                value /= 10.0;
-            }
-            else value = value * Math.Pow(10, power);
-
-            if (double.IsPositiveInfinity(value) || double.IsNegativeInfinity(value)) throw new Exception();
-
-            if (isNeg && value > 0) value = -value;
-
-            return value;
-        }
-
-        private static long ToInt64(string value, bool signed, long min, long max)
-        {
-            if (value == null) return 0;
-
-            value = value.Trim(' ');
-
-            var num = value.ToCharArray();
-            var len = num.Length;
-            ulong result = 0;
-            var index = 0;
-            var isNeg = false;
-
-            // check the sign
-            if (num[0] == '-')
-            {
-                isNeg = true;
-                index = 1;
-            }
-            else if (num[0] == '+')
-            {
-                index = 1;
-            }
-
-            for (var i = index; i < len; i++)
-            {
-                ulong digit;
-                var c = num[i];
-
-                // switch statement is faster than subtracting '0'
-                switch (c)
-                {
-                    case '0':
-                    digit = 0;
-                    break;
-                    case '1':
-                    digit = 1;
-                    break;
-                    case '2':
-                    digit = 2;
-                    break;
-                    case '3':
-                    digit = 3;
-                    break;
-                    case '4':
-                    digit = 4;
-                    break;
-                    case '5':
-                    digit = 5;
-                    break;
-                    case '6':
-                    digit = 6;
-                    break;
-                    case '7':
-                    digit = 7;
-                    break;
-                    case '8':
-                    digit = 8;
-                    break;
-                    case '9':
-                    digit = 9;
-                    break;
-                    default:
-                    throw new Exception();
-                }
-
-                // check for overflow - any number greater than this number will cause an overflow
-                // when multiplied by 10
-                if (signed && result > 0x0CCCCCCCCCCCCCCC || !signed && result > 0x1999999999999999) throw new Exception();
-
-                result *= 10;
-                result += digit;
-            }
-
-            if (isNeg && !signed && result != 0) throw new Exception();
-
-            long res;
-
-            if (isNeg)
-            {
-                res = -(long)result;
-
-                // if the result is not negative, we had an overflow
-                if (res > 0) throw new Exception();
-            }
-            else
-            {
-                res = (long)result;
-
-                // if the result is negative and we are not converting a
-                // UInt64, we had an overflow
-                if (max != 0 && res < 0) throw new Exception();
-            }
-
-            // final check for max/min
-            if (max != 0 && (res < min || res > max)) throw new Exception();
-
-            return res;
-        }
-
-        private static double GetDoubleNumber(char[] chars, int start, int length, out int numLeadingZeros)
-        {
-            double number = 0;
-            var isNeg = false;
-            var end = start + length;
-
-            numLeadingZeros = 0;
-
-            if (chars[start] == '-')
-            {
-                isNeg = true;
-                start++;
-            }
-            else if (chars[start] == '+')
-            {
-                start++;
-            }
-
-            for (var i = start; i < end; i++)
-            {
-                int digit;
-                var c = chars[i];
-
-                // switch statement is faster than subtracting '0'                
-                switch (c)
-                {
-                    case '0':
-                    // update the number of leading zeros (used for normalizing)
-                    if (numLeadingZeros + start == i)
-                    {
-                        numLeadingZeros++;
-                    }
-                    digit = 0;
-                    break;
-                    case '1':
-                    digit = 1;
-                    break;
-                    case '2':
-                    digit = 2;
-                    break;
-                    case '3':
-                    digit = 3;
-                    break;
-                    case '4':
-                    digit = 4;
-                    break;
-                    case '5':
-                    digit = 5;
-                    break;
-                    case '6':
-                    digit = 6;
-                    break;
-                    case '7':
-                    digit = 7;
-                    break;
-                    case '8':
-                    digit = 8;
-                    break;
-                    case '9':
-                    digit = 9;
-                    break;
-                    default:
-                    throw new Exception();
-                }
-
-                number *= 10;
-                number += digit;
-            }
-
-            return isNeg ? -number : number;
+            return NativeToDouble(s);
         }
 
         /// <summary>
