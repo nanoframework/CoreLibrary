b6e85e61e1c0012a172f4243ecff94a2b8349f0e
José Simões <jose.simoes@eclo.solutions>
2018-09-28 14:44:19 +0100

    Work on CI
    - Update git version config file
      + fix assemblyinfo file path: was creating a new assembly info file instead of updating the exising one
      + syntax changes required in last version
      + next version set to 1.0.0 (again)
      + remove requirement to increment only on commit message
    - Update generate log PS to generate on all builds except PRs
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- generate-change-log.ps1
+++ generate-change-log.ps1
@@ -1,20 +1,16 @@
-# generate cahnge log on tag commit OR when building in master (for releases)
-if ($env:appveyor_repo_tag -eq "true" -or $env:appveyor_repo_branch -eq "master")
+# generate change log when build is NOT for a pull-request
+if ($env:appveyor_pull_request_number)
+{
+    'Skip change log processing as this is a PR build...' | Write-Host -ForegroundColor White
+}
+else
 {
     # need this to keep ruby happy
     md c:\tmp
 
     # generate change log
-    if ($env:appveyor_repo_tag -eq "true")
-    {   
-        # version includes commits
-        bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:GitVersion_MajorMinorPatch.$env:GitVersion_CommitsSinceVersionSource"
-    }
-    else
-    {
-        # release version, just the regular version format
-        bundle exec github_changelog_generator --token $env:GitHubToken --future-release "v$env:GitVersion_MajorMinorPatch"
-    }
+    # version includes commits
+    bundle exec github_changelog_generator --token $env:GitHubToken
 
     # updated changelog and the updated assembly info files
     git add CHANGELOG.md
