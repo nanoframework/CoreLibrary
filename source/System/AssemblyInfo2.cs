536191a59e9f23af541d52dfa7ecaf6a5dba4d90
nfbot <nfbot@users.noreply.github.com>
2018-09-28 16:49:25 +0000

    Update assembly info file for v1.0.1-preview091

--- source/System/AssemblyInfo2.cs
+++ source/System/AssemblyInfo2.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // Copyright (c) 2017 The nanoFramework project contributors
 // Portions Copyright (c) Microsoft Corporation.  All rights reserved.
 // See LICENSE file in the project root for full license information.
@@ -12,6 +12,6 @@ using System.Reflection;
 [assembly: AssemblyCompany("nanoFramework Contributors")]
 [assembly: AssemblyProduct("nanoFramework mscorlib")]
 [assembly: AssemblyCopyright("Copyright © nanoFramework Contributors 2017")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.91")]
+[assembly: AssemblyFileVersion("1.0.1.91")]
+[assembly: AssemblyInformationalVersion("1.0.1-preview091")]
