8965b4508a2909a60688d38b9b1ba5a2b7be0c65
José Simões <josesimoes@users.noreply.github.com>
2018-09-28 17:03:45 +0100

    Update generate-change-log.ps1

--- generate-change-log.ps1
+++ generate-change-log.ps1
@@ -14,7 +14,7 @@ else
 
     # updated changelog and the updated assembly info files
     git add CHANGELOG.md
-    git commit -m "Update CHANGELOG for v$env:GitVersion_MajorMinorPatch.$env:GitVersion_CommitsSinceVersionSource"
+    git commit -m "Update CHANGELOG for v$env:GitVersion_NuGetVersionV2"
     # need to wrap the git command bellow so it doesn't throw an error because of redirecting the output to stderr
     "$(git push origin)"
 }
