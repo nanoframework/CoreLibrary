c2074e4061fbde40e44b6e89fd1827e10b3d30aa
José Simões <jose.simoes@eclo.solutions>
2018-09-28 10:41:07 +0100

    Work on CI
    - Add change log generator PS and config file
    - Update AppVeyor in preparation for RC and change log generator
    - Update PS1 for VS extension install
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -1,4 +1,9 @@
 $vsixPath = "$($env:USERPROFILE)\nanoFramework.Tools.VS2017.Extension.vsix"
 (New-Object Net.WebClient).DownloadFile('https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vs-publisher-1470366/vsextensions/nanoFrameworkVS2017Extension/0/vspackage', $vsixPath)
 "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
-& .\install-vsix.cmd
+
+'Installing nanoFramework VS extension ...' | Write-Host -ForegroundColor White -NoNewline
+
+& .\install-vsix.cmd --quiet --no-verbose > $null
+
+'OK' | Write-Host -ForegroundColor Green
