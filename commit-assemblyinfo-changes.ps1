fc21fb6b073a2149446038e43d1834547685ea0e
José Simões <josesimoes@users.noreply.github.com>
2018-10-01 17:34:41 +0100

    Work on CI (#34)

--- commit-assemblyinfo-changes.ps1
+++ commit-assemblyinfo-changes.ps1
@@ -8,6 +8,64 @@ else
     # updated assembly info files
     git add "source\System\AssemblyInfo2.cs"
     git commit -m "Update assembly info file for v$env:GitVersion_NuGetVersionV2"
-    # need to wrap the git command bellow so it doesn't throw an error because of redirecting the output to stderr
-    "$(git push origin)"
+    git push origin --porcelain -q > $null
+        
+    'Updating assembly info...' | Write-Host -ForegroundColor White -NoNewline
+    'OK' | Write-Host -ForegroundColor Green
+
+    # clone nf-interpreter repo (only a shallow clone with last commit)
+    git clone https://github.com/nanoframework/nf-interpreter -b develop --depth 1 -q
+    cd nf-interpreter
+
+    # new branch name
+    $newBranch = "nfbot/$env:APPVEYOR_REPO_BRANCH/update-version/$env:GitVersion_NuGetVersionV2" 
+    
+    # create branch to perform updates
+    git checkout -b "$newBranch" develop -q
+    
+    # replace version in assembly declaration
+    $newVersion = $env:GitVersion_AssemblySemFileVer -replace "\." , ", "
+    $newVersion = "{ $newVersion }"
+    
+    $versionRegex = "\{\s*\d+\,\s*\d+\,\s*\d+\,\s*\d+\s*}"
+    $assemblyFiles = (Get-ChildItem ".\src\CLR\CorLib\corlib_native.cpp" -Recurse)
+
+    foreach($file in $assemblyFiles)
+    {
+        $filecontent = Get-Content($file)
+        attrib $file -r
+        $filecontent -replace $versionRegex, $newVersion | Out-File $file -Encoding utf8
+    }
+
+    $commitMessage = "Update nanoFramework.CorLib version to $env:GitVersion_AssemblySemFileVer"
+
+    # commit changes
+    git add -A 2>&1
+    git commit -m"$commitMessage" -m"[version update]" -q
+    git push --set-upstream origin "$newBranch" --porcelain -q > $null
+ 
+    # start PR
+    $prRequestBody = @{title="$commitMessage";body="$commitMessage`n[version update]";head="$newBranch";base="develop"} | ConvertTo-Json
+    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-interpreter/pulls"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    try 
+    {
+        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header @{"Authorization"="Basic $env:GitRestAuth"} -ContentType "application/json" -Body $prRequestBody
+        'Starting PR with version update...' | Write-Host -ForegroundColor White -NoNewline
+        'OK' | Write-Host -ForegroundColor Green
+    }
+    catch 
+    {
+        $result = $_.Exception.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($result)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+
+        "Error starting PR: $responseBody" | Write-Host -ForegroundColor Red
+    }
+
+    # move back to home folder
+    &  cd .. > $null
 }
