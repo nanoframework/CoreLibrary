854168f406f6c96f2d0400529f6b9dddc7923f67
José Simões <josesimoes@users.noreply.github.com>
2018-10-01 17:46:47 +0100

    Update branch name for version update (#35)

--- commit-assemblyinfo-changes.ps1
+++ commit-assemblyinfo-changes.ps1
@@ -18,7 +18,7 @@ else
     cd nf-interpreter
 
     # new branch name
-    $newBranch = "nfbot/$env:APPVEYOR_REPO_BRANCH/update-version/$env:GitVersion_NuGetVersionV2" 
+    $newBranch = "$env:APPVEYOR_REPO_BRANCH/nfbot/update-version/$env:GitVersion_NuGetVersionV2" 
     
     # create branch to perform updates
     git checkout -b "$newBranch" develop -q
