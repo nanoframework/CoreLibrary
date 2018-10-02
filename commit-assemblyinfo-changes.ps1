77f8b66d4c9f478b871bf29a250c7b303e60e8cb
José Simões <jose.simoes@eclo.solutions>
2018-10-02 18:45:52 +0100

    Repo reorganization
    - Moved library source files to its own folder
    - Add Nuproj projects to main solution
    - Add msbuild property to allow building Nuproj projects locally when GitVersion is not available
    - Add NuGet.config
    - Remove deprecated .nuget folder from solution
    - Drop solution specific for NuGets
    - Update CI scripts accordingly
    - Remove rsp files from git ignore
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- commit-assemblyinfo-changes.ps1
+++ commit-assemblyinfo-changes.ps1
@@ -6,7 +6,7 @@ if ($env:appveyor_pull_request_number)
 else
 {
     # updated assembly info files
-    git add "source\System\AssemblyInfo2.cs"
+    git add "source\nanoFramework.CoreLibrary\System\AssemblyInfo2.cs"
     git commit -m "Update assembly info file for v$env:GitVersion_NuGetVersionV2"
     git push origin --porcelain -q > $null
         
@@ -37,35 +37,45 @@ else
         $filecontent -replace $versionRegex, $newVersion | Out-File $file -Encoding utf8
     }
 
-    $commitMessage = "Update nanoFramework.CorLib version to $env:GitVersion_NuGetVersion"
+    # check if anything was changed
+    $repoStatus = "$(git status --short --porcelain)"
 
-    # commit changes
-    git add -A 2>&1
-    git commit -m"$commitMessage" -m"[version update]" -q
-    git push --set-upstream origin "$newBranch" --porcelain -q > $null
- 
-    # start PR
-    $prRequestBody = @{title="$commitMessage";body="$commitMessage`nStarted with https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT`n[version update]";head="$newBranch";base="develop"} | ConvertTo-Json
-    $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-interpreter/pulls"
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-    try 
+    if ($repoStatus -eq "") 
     {
-        $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header @{"Authorization"="Basic $env:GitRestAuth"} -ContentType "application/json" -Body $prRequestBody
-        'Starting PR with version update...' | Write-Host -ForegroundColor White -NoNewline
-        'OK' | Write-Host -ForegroundColor Green
+        # nothting changed
     }
-    catch 
+    else
     {
-        $result = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($result)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
+        $commitMessage = "Update nanoFramework.CorLib version to $env:GitVersion_NuGetVersion"
 
-        "Error starting PR: $responseBody" | Write-Host -ForegroundColor Red
+        # commit changes
+        git add -A 2>&1
+        git commit -m"$commitMessage" -m"[version update]" -q
+        git push --set-upstream origin "$newBranch" --porcelain -q > $null
+    
+        # start PR
+        $prRequestBody = @{title="$commitMessage";body="$commitMessage`nStarted with https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT`n[version update]";head="$newBranch";base="develop"} | ConvertTo-Json
+        $githubApiEndpoint = "https://api.github.com/repos/nanoframework/nf-interpreter/pulls"
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+        try 
+        {
+            $result = Invoke-RestMethod -Method Post -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer -Uri  $githubApiEndpoint -Header @{"Authorization"="Basic $env:GitRestAuth"} -ContentType "application/json" -Body $prRequestBody
+            'Starting PR with version update...' | Write-Host -ForegroundColor White -NoNewline
+            'OK' | Write-Host -ForegroundColor Green
+        }
+        catch 
+        {
+            $result = $_.Exception.Response.GetResponseStream()
+            $reader = New-Object System.IO.StreamReader($result)
+            $reader.BaseStream.Position = 0
+            $reader.DiscardBufferedData()
+            $responseBody = $reader.ReadToEnd();
+
+            "Error starting PR: $responseBody" | Write-Host -ForegroundColor Red
+        }
+
+        # move back to home folder
+        &  cd .. > $null
     }
-
-    # move back to home folder
-    &  cd .. > $null
 }
