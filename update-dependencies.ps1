51f1673975eb23468d266bbc1f92d8c7278496a8
José Simões <josesimoes@users.noreply.github.com>
2018-10-08 13:48:00 +0100

    Update update-dependencies.ps1

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -10,6 +10,10 @@ else
 {
     # update dependencies for class libraries that depend ONLY on mscorlib
 
+    # because it can take sometime for the package to become available on the NuGet providers
+    # need to hange here for 5 minutes
+    Start-Sleep -Milliseconds 5000
+
     $librariesToUpdate =    ("lib-nanoFramework.Runtime.Events", 
                             "lib-nanoFramework.Runtime.Native",
                             "lib-Windows.Storage.Streams",
