d36b9677a980bf9c0dd5be9b8785fb7205694e77
José Simões <josesimoes@users.noreply.github.com>
2018-10-15 15:58:42 +0100

    Update update-dependencies.ps1

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -45,7 +45,7 @@ else
         $solutionFile = (Get-ChildItem -Path ".\" -Include "*.sln" -Recurse)
 
         # run NuKeeper inspect
-        if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $string -like '*master*')
+        if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $env:APPVEYOR_REPO_BRANCH-like '*master*')
         {
             # use NuGet ONLY for release and master branches
             $nukeeperInspect = NuKeeper inspect --source https://api.nuget.org/v3/index.json
@@ -70,7 +70,7 @@ else
             [array]$packageList = $packageListRaw.Split([Environment]::NewLine, [StringSplitOptions]::RemoveEmptyEntries).Replace([Environment]::NewLine, "")
 
             # restore NuGet packages, need to do this before anything else
-            if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $string -like '*master*')
+            if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $env:APPVEYOR_REPO_BRANCH -like '*master*')
             {
                 # use NuGet ONLY for release and master branches
                 nuget restore $solutionFile[0] -Source https://api.nuget.org/v3/index.json
@@ -99,7 +99,7 @@ else
                 $packageTargetVersion = $packageDetails.captures.Groups[6].Value.Trim();
     
                 # update package
-                if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $string -like '*master*')
+                if ($env:APPVEYOR_REPO_BRANCH -like '*release*' -or $env:APPVEYOR_REPO_BRANCH -like '*master*')
                 {
                     # use NuGet ONLY for release and master branches
                     $updatePackage = nuget update $solutionFile[0].FullName -Source https://api.nuget.org/v3/index.json
