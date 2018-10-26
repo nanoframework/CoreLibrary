a737c11d6cd9bb599b88b4b601aae6ae5b259c3b
nfbot <nfbot@users.noreply.github.com>
2018-10-26 02:33:02 +0000

    Update assembly info file for v1.0.5-preview023
    [version update]

--- source/nanoFramework.CoreLibrary/System/AssemblyInfo2.cs
+++ source/nanoFramework.CoreLibrary/System/AssemblyInfo2.cs
@@ -15,4 +15,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("1.0.5-preview019")]
+[assembly: AssemblyInformationalVersion("1.0.5-preview023")]
