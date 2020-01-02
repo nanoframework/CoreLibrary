0aad719c0025c7fe541b7f057cff9bde07763f3f
José Simões <jose.simoes@eclo.solutions>
2020-01-02 14:20:47 +0000

    Implement MemberInfo.GetCustomAttributesNative
    - Bump AssemblyNativeVersion to 100.4.7.0.
    - Bump version to 1.7.0-preview.
    
    Signed-off-by: José Simões <jose.simoes@eclo.solutions>

--- source/nanoFramework.CoreLibrary/System/AssemblyInfo.cs
+++ source/nanoFramework.CoreLibrary/System/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("nanoFramework mscorlib")]
 [assembly: AssemblyCopyright("Copyright © nanoFramework Contributors 2017")]
 
-[assembly: AssemblyNativeVersion("100.4.6.0")]
+[assembly: AssemblyNativeVersion("100.4.7.0")]
