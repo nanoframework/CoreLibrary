b1afea0712319644072ad9e5647f03f5983c6d73
josesimoes <jose.simoes@eclo.solutions>
2020-10-20 15:13:36 +0100

    Add Enum.HasFlag
    - Bump AssemblyNativeVersion to 100.5.0.2.
    - Bump version to 1.9.1-preview.

--- nanoFramework.CoreLibrary/System/Enum.cs
+++ nanoFramework.CoreLibrary/System/Enum.cs
@@ -4,6 +4,8 @@
 // See LICENSE file in the project root for full license information.
 //
 
+using System.Runtime.CompilerServices;
+
 namespace System
 {
     /// <summary>
@@ -12,6 +14,14 @@ namespace System
     [Serializable]
     public abstract class Enum : ValueType
     {
+        /// <summary>
+        /// Determines whether one or more bit fields are set in the current instance.
+        /// </summary>
+        /// <param name="flag">An enumeration value.</param>
+        /// <returns><see langword="true"/> if the bit field or bit fields that are set in flag are also set in the current instance; otherwise, <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentException"><paramref name="flag"/> is a different type than the current instance.</exception>
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern bool HasFlag(Enum flag);
 
         /// <summary>
         /// Converts the value of this instance to its equivalent string representation.
