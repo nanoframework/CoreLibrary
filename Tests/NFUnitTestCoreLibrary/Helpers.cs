b869b003ed33bb705baff08f7e01539dc3ac8f3e
Laurent Ellerbach <laurelle@microsoft.com>
2021-02-28 12:56:21 +0300

    adding thread part 1

--- Tests/NFUnitTestCoreLibrary/Helpers.cs
+++ Tests/NFUnitTestCoreLibrary/Helpers.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
 
 namespace NFUnitTestCoreLibrary
 {
