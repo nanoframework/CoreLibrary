4f28bb680c5b9810466952dc47b73604e0de5543
Laurent Ellerbach <laurelle@microsoft.com>
2021-03-01 23:18:28 +0300

    Adding attribute tests

--- Tests/NFUnitTestBasicConcepts/UnitTestBasicConceptsTests.cs
+++ Tests/NFUnitTestBasicConcepts/UnitTestBasicConceptsTests.cs
@@ -1,3 +1,9 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
 using nanoFramework.TestFramework;
 using System;
 using System.Diagnostics;
