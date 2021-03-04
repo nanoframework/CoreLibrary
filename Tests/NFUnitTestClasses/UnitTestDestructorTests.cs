194eba1f1ca805b9a2b88ec35952cbe1025819f7
Laurent Ellerbach <laurelle@microsoft.com>
2021-03-04 11:47:48 +0300

    Removing non mscorlib references

--- Tests/NFUnitTestClasses/UnitTestDestructorTests.cs
+++ Tests/NFUnitTestClasses/UnitTestDestructorTests.cs
@@ -14,17 +14,18 @@ namespace NFUnitTestClasses
     [TestClass]
     class UnitTestDestructorTests
     {
-        [TestMethod]
-        public void Destructors3_Test()
-        {
-            //Ported from Destructors3.cs
-            Debug.WriteLine(" Section 10.11");
-            Debug.WriteLine(" Destructors implement the actions required to ");
-            Debug.WriteLine(" destruct the instances of a class.");
-            Debug.WriteLine("");
-            Debug.WriteLine("Note: This test may fail due to lengthy garbage collection, look for Destructor messages in later logs");
-            Assert.True(DestructorsTestClass3.testMethod());
-        }
+        // Removing as using something out of mscorlib
+        //[TestMethod]
+        //public void Destructors3_Test()
+        //{
+        //    //Ported from Destructors3.cs
+        //    Debug.WriteLine(" Section 10.11");
+        //    Debug.WriteLine(" Destructors implement the actions required to ");
+        //    Debug.WriteLine(" destruct the instances of a class.");
+        //    Debug.WriteLine("");
+        //    Debug.WriteLine("Note: This test may fail due to lengthy garbage collection, look for Destructor messages in later logs");
+        //    Assert.True(DestructorsTestClass3.testMethod());
+        //}
 
         [TestMethod]
         public void Destructors4_Test()
@@ -38,54 +39,55 @@ namespace NFUnitTestClasses
             Assert.True(DestructorsTestClass4.testMethod());
         }
 
-        [TestMethod]
-        public void Destructors7_Test()
-        {
-            //Ported from Destructors7.cs
-            Debug.WriteLine(" Section 10.12");
-            Debug.WriteLine(" Destructors are not inherited. Thus, a class");
-            Debug.WriteLine(" has no other destructors than those that are ");
-            Debug.WriteLine(" actually declared in the class.");
-            Debug.WriteLine("");
-            Debug.WriteLine("Note: This test may fail due to lengthy garbage collection, look for Destructor messages in later logs");
-            Assert.True(DestructorsTestClass7.testMethod());
-        }
+        // Removed as using a class out of mscorlib
+        //[TestMethod]
+        //public void Destructors7_Test()
+        //{
+        //    //Ported from Destructors7.cs
+        //    Debug.WriteLine(" Section 10.12");
+        //    Debug.WriteLine(" Destructors are not inherited. Thus, a class");
+        //    Debug.WriteLine(" has no other destructors than those that are ");
+        //    Debug.WriteLine(" actually declared in the class.");
+        //    Debug.WriteLine("");
+        //    Debug.WriteLine("Note: This test may fail due to lengthy garbage collection, look for Destructor messages in later logs");
+        //    Assert.True(DestructorsTestClass7.testMethod());
+        //}
 
-        class DestructorsTestClass3
-        {
+        //class DestructorsTestClass3
+        //{
 
-            static int intI = 1;
+        //    static int intI = 1;
 
-            ~DestructorsTestClass3()
-            {
-                //Debug.WriteLine("Calling Destructor for Test Class 3");
-                intI = 2;
-            }
+        //    ~DestructorsTestClass3()
+        //    {
+        //        //Debug.WriteLine("Calling Destructor for Test Class 3");
+        //        intI = 2;
+        //    }
 
-            public static bool testMethod()
-            {
-                DestructorsTestClass3 mc = new DestructorsTestClass3();
-                mc = null;
-                nanoFramework.Runtime.Native.GC.Run(true);
-                int sleepTime = 5000;
-                int slept = 0;
-                while (intI != 2 && slept < sleepTime)
-                {
-                    System.Threading.Thread.Sleep(10);
-                    slept += 10;
-                }
-                Debug.WriteLine("Thread has slept for");
-                Debug.WriteLine(slept.ToString());
-                if (intI == 2)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
+        //    public static bool testMethod()
+        //    {
+        //        DestructorsTestClass3 mc = new DestructorsTestClass3();
+        //        mc = null;
+        //        nanoFramework.Runtime.Native.GC.Run(true);
+        //        int sleepTime = 5000;
+        //        int slept = 0;
+        //        while (intI != 2 && slept < sleepTime)
+        //        {
+        //            System.Threading.Thread.Sleep(10);
+        //            slept += 10;
+        //        }
+        //        Debug.WriteLine("Thread has slept for");
+        //        Debug.WriteLine(slept.ToString());
+        //        if (intI == 2)
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
 
 
         class DestructorsTestClass4_Base
@@ -111,7 +113,7 @@ namespace NFUnitTestClasses
             {
                 DestructorsTestClass4 mc = new DestructorsTestClass4();
                 mc = null;
-                nanoFramework.Runtime.Native.GC.Run(true);
+                // nanoFramework.Runtime.Native.GC.Run(true);
                 int sleepTime = 5000;
                 int slept = 0;
                 while (intI != 8 && slept < sleepTime)
@@ -150,7 +152,7 @@ namespace NFUnitTestClasses
             {
                 DestructorsTestClass7 mc = new DestructorsTestClass7();
                 mc = null;
-                nanoFramework.Runtime.Native.GC.Run(true);
+                //nanoFramework.Runtime.Native.GC.Run(true);
                 int sleepTime = 5000;
                 int slept = 0;
                 while (intI != 3 && slept < sleepTime)
