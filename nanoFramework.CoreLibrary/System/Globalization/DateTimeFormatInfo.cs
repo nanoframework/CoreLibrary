1508b6b685fb14744220a2540b567a6a6d883a69
José Simões <jose.simoes@eclo.solutions>
2021-10-13 12:15:09 +0100

    Improvements and fixes in DateTimeFormatInfo
    - Remove duplicated strings.
    - Tidy-up code.
    - Add thorough unit tests for DateTime.ToString with predefined formats.

--- nanoFramework.CoreLibrary/System/Globalization/DateTimeFormatInfo.cs
+++ nanoFramework.CoreLibrary/System/Globalization/DateTimeFormatInfo.cs
@@ -8,19 +8,13 @@
 #define ENABLE_CROSS_APPDOMAIN
 namespace System.Globalization
 {
-    using System;
-
     /// <summary>
     /// Provides culture-specific information about the format of date and time values.
     /// </summary>
     public sealed class DateTimeFormatInfo /*: ICloneable, IFormatProvider*/
     {
-        internal String _generalShortTimePattern = "MM/dd/yyyy HH:mm";
-        internal String _generalLongTimePattern = "MM/dd/yyyy HH:mm:ss";
-        internal const String _rfc1123Pattern = "ddd, dd MMM yyyy HH':'mm':'ss 'GMT'";
-        internal const String _sortableDateTimePattern = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
-        internal const String _universalSortableDateTimePattern = "yyyy'-'MM'-'dd HH':'mm':'ss'Z'";
-        internal String _fullDateTimePattern = "dddd, dd MMMM yyyy HH:mm:ss";
+        internal const string _sortableDatePattern = "yyyy'-'MM'-'dd";
+        internal const string _sortableTimePattern = "HH':'mm':'ss";
 
         internal DateTimeFormatInfo(CultureInfo cultureInfo)
         {
@@ -42,7 +36,7 @@ namespace System.Globalization
         /// Gets the string designator for hours that are "ante meridiem" (before noon).
         /// </summary>
         /// <value>The string designator for hours that are ante meridiem. The default for InvariantInfo is "AM".</value>
-        public String AMDesignator
+        public string AMDesignator
         {
             get
             {
@@ -54,7 +48,7 @@ namespace System.Globalization
         /// Gets the string that separates the components of a date, that is, the year, month, and day.
         /// </summary>
         /// <value>The string that separates the components of a date, that is, the year, month, and day. The default for InvariantInfo is "/".</value>
-        public String DateSeparator
+        public string DateSeparator
         {
             get
             {
@@ -66,11 +60,11 @@ namespace System.Globalization
         /// Gets the custom format string for a long date and long time value.
         /// </summary>
         /// <value>The custom format string for a long date and long time value.</value>
-        public String FullDateTimePattern
+        public string FullDateTimePattern
         {
             get
             {
-                return _fullDateTimePattern;
+                return LongDatePattern + " " + LongTimePattern;
             }
         }
 
@@ -78,7 +72,7 @@ namespace System.Globalization
         /// Gets the custom format string for a long date value.
         /// </summary>
         /// <value>The custom format string for a long date value.</value>
-        public String LongDatePattern
+        public string LongDatePattern
         {
             get
             {
@@ -90,7 +84,7 @@ namespace System.Globalization
         /// Gets the custom format string for a long time value.
         /// </summary>
         /// <value>The format pattern for a long time value.</value>
-        public String LongTimePattern
+        public string LongTimePattern
         {
             get
             {
@@ -102,7 +96,7 @@ namespace System.Globalization
         /// Gets the custom format string for a month and day value.
         /// </summary>
         /// <value>The custom format string for a month and day value.</value>
-        public String MonthDayPattern
+        public string MonthDayPattern
         {
             get
             {
@@ -114,7 +108,7 @@ namespace System.Globalization
         /// Gets the string designator for hours that are "post meridiem" (after noon).
         /// </summary>
         /// <value>The string designator for hours that are "post meridiem" (after noon). The default for InvariantInfo is "PM".</value>
-        public String PMDesignator
+        public string PMDesignator
         {
             get
             {
@@ -126,11 +120,11 @@ namespace System.Globalization
         /// Gets the custom format string for a time value that is based on the Internet Engineering Task Force (IETF) Request for Comments (RFC) 1123 specification.
         /// </summary>
         /// <value>The custom format string for a time value that is based on the IETF RFC 1123 specification.</value>
-        public String RFC1123Pattern
+        public string RFC1123Pattern
         {
             get
             {
-                return _rfc1123Pattern;
+                return "ddd, dd MMM yyyy " + _sortableTimePattern + " 'GMT'";
             }
         }
 
@@ -138,7 +132,7 @@ namespace System.Globalization
         /// Gets the custom format string for a short date value.
         /// </summary>
         /// <value>The custom format string for a short date value.</value>
-        public String ShortDatePattern
+        public string ShortDatePattern
         {
             get
             {
@@ -150,7 +144,7 @@ namespace System.Globalization
         /// Gets the custom format string for a short time value.
         /// </summary>
         /// <value>The custom format string for a short time value.</value>
-        public String ShortTimePattern
+        public string ShortTimePattern
         {
             get
             {
@@ -162,27 +156,27 @@ namespace System.Globalization
         /// Gets the custom format string for a sortable date and time value.
         /// </summary>
         /// <value>The custom format string for a sortable date and time value.</value>
-        public String SortableDateTimePattern
+        public string SortableDateTimePattern
         {
             get
             {
-                return _sortableDateTimePattern;
+                return _sortableDatePattern + "'T'" + _sortableTimePattern;
             }
         }
 
-        internal String GeneralShortTimePattern
+        internal string GeneralShortTimePattern
         {
             get
             {
-                return _generalShortTimePattern;
+                return ShortDatePattern + " " + ShortTimePattern;
             }
         }
 
-        internal String GeneralLongTimePattern
+        internal string GeneralLongTimePattern
         {
             get
             {
-                return _generalLongTimePattern;
+                return ShortDatePattern + " " + LongTimePattern;
             }
         }
 
@@ -190,7 +184,7 @@ namespace System.Globalization
         /// Gets the string that separates the components of time, that is, the hour, minutes, and seconds.
         /// </summary>
         /// <value>The string that separates the components of time. The default for InvariantInfo is ":".</value>
-        public String TimeSeparator
+        public string TimeSeparator
         {
             get
             {
@@ -202,11 +196,11 @@ namespace System.Globalization
         /// Gets the custom format string for a universal, sortable date and time string.
         /// </summary>
         /// <value>The custom format string for a universal, sortable date and time string.</value>
-        public String UniversalSortableDateTimePattern
+        public string UniversalSortableDateTimePattern
         {
             get
             {
-                return _universalSortableDateTimePattern;
+                return _sortableDatePattern + " " + _sortableTimePattern + "'Z'";
             }
         }
 
@@ -214,7 +208,7 @@ namespace System.Globalization
         /// Gets the custom format string for a year and month value.
         /// </summary>
         /// <value>The custom format string for a year and month value.</value>
-        public String YearMonthPattern
+        public string YearMonthPattern
         {
             get
             {
@@ -226,11 +220,11 @@ namespace System.Globalization
         /// Gets a one-dimensional array of type String containing the culture-specific abbreviated names of the days of the week.
         /// </summary>
         /// <value>A one-dimensional array of type String containing the culture-specific abbreviated names of the days of the week. The array for InvariantInfo contains "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", and "Sat".</value>
-        public String[] AbbreviatedDayNames
+        public string[] AbbreviatedDayNames
         {
             get
             {
-                return new String[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+                return new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
             }
         }
 
@@ -238,11 +232,11 @@ namespace System.Globalization
         /// Gets a one-dimensional string array that contains the culture-specific full names of the days of the week.
         /// </summary>
         /// <value>A one-dimensional string array that contains the culture-specific full names of the days of the week. The array for InvariantInfo contains "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", and "Saturday".</value>
-        public String[] DayNames
+        public string[] DayNames
         {
             get
             {
-                return new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+                return new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
             }
         }
 
@@ -251,11 +245,11 @@ namespace System.Globalization
         /// </summary>
         /// <value>A one-dimensional string array with 13 elements that contains the culture-specific abbreviated names of the months. For 12-month calendars, the 13th element of the array is an empty string. 
         /// The array for InvariantInfo contains "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", and "".</value>
-        public String[] AbbreviatedMonthNames
+        public string[] AbbreviatedMonthNames
         {
             get
             {
-                return new String[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "" };
+                return new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "" };
             }
         }
 
@@ -264,11 +258,11 @@ namespace System.Globalization
         /// </summary>
         /// <value>A one-dimensional array of type String containing the culture-specific full names of the months. In a 12-month calendar, the 13th element of the array is an empty string. 
         /// The array for InvariantInfo contains "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", and "".</value>
-        public String[] MonthNames
+        public string[] MonthNames
         {
             get
             {
-                return new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "" };
+                return new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "" };
             }
         }
     }
