9c5f2e3f7681fa52c1d9e17e9a956058d5331397
José Simões <jose.simoes@eclo.solutions>
2021-11-20 11:54:22 +0000

    Add Console.Write and Console.WriteLine (#162)
    ***NO_CI***

--- nanoFramework.CoreLibrary/System/Console.cs
+++ nanoFramework.CoreLibrary/System/Console.cs
@@ -5,10 +5,39 @@
 
 namespace System
 {
-    internal static class Console
+    /// <summary>
+    /// Writes the specified data, followed by the current line terminator, to the standard output stream.
+    /// </summary>
+    /// <remarks>
+    /// The default line terminator is a string whose value is a carriage return followed by a line feed ("\r\n" in C#, or vbCrLf in Visual Basic).
+    /// In .NET nanoFramework this will write to Visual Studio Output window.
+    /// </remarks>
+    public static class Console
     {
-        internal static void Write(string value) => Diagnostics.Debug.WriteLineNative(value, false);
+        /// <summary>
+        /// Writes the specified string value, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <remarks>
+        /// In .NET nanoFramework this will write to Visual Studio Output window.
+        /// </remarks>
+        public static void Write(string value) => Diagnostics.Debug.WriteLineNative(value, false);
 
-        internal static void WriteLine(string value) => Diagnostics.Debug.WriteLineNative(value, true);
+        /// <summary>
+        /// Writes the current line terminator to the standard output stream.
+        /// </summary>
+        /// <remarks>
+        /// In .NET nanoFramework this will write to Visual Studio Output window.
+        /// </remarks>
+        public static void WriteLine() => Diagnostics.Debug.WriteLineNative(string.Empty, true);
+
+        /// <summary>
+        /// Writes the specified string value, followed by the current line terminator, to the standard output stream.
+        /// </summary>
+        /// <param name="value">The value to write.</param>
+        /// <remarks>
+        /// In .NET nanoFramework this will write to Visual Studio Output window.
+        /// </remarks>
+        public static void WriteLine(string value) => Diagnostics.Debug.WriteLineNative(value, true);
     }
 }
