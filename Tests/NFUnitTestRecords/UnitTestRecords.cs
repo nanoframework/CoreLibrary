b7a3f9ea37ed96d42ecb518388c83e5769914297
José Simões <jose.simoes@eclo.solutions>
2021-11-23 08:53:23 +0000

    Add IsExternalInit
    - Add unit tests for Records (disabled until generics are supported).

---
+++ Tests/NFUnitTestRecords/UnitTestRecords.cs
@@ -0,0 +1,45 @@
+using nanoFramework.TestFramework;
+using System;
+
+namespace NFUnitTestRecords
+{
+    //[TestClass]
+    //public class UnitTestRecords
+    //{
+    //    [TestMethod]
+    //    public void Records_Test00()
+    //    {
+    //        var firstName = "Test";
+    //        var lastName = "Person";
+    //        var dob = new DateTime(1998, 1, 31);
+    //        var id = Guid.NewGuid();
+    //        var emptyGuid = Guid.Empty;
+            
+    //        var newPerson = new Person(firstName, lastName, dob)
+    //        {
+    //            Id = emptyGuid,
+    //        };
+
+    //        var expectedPerson = new Person(firstName, lastName, dob)
+    //        {
+    //            Id = id,
+    //        };
+    //    }
+    //}
+
+    //public record Person
+    //{
+    //    public Person(string firstName, string lastName, DateTime
+    //    dateOfBirth)
+    //    {
+    //        FirstName = firstName;
+    //        LastName = lastName;
+    //        DateOfBirth = dateOfBirth;
+    //    }
+
+    //    public Guid Id { get; set; }
+    //    public string FirstName { get; }
+    //    public string LastName { get; }
+    //    public DateTime DateOfBirth { get; }
+    //}
+}
