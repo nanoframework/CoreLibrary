eee9db8624a19d2d115632d66b2397c14e2e72d8
José Simões <jose.simoes@eclo.solutions>
2021-11-23 12:08:18 +0000

    Add IsExternalInit (#163)
    ***NO_CI***

---
+++ nanoFramework.CoreLibrary/System/Runtime/CompilerServices/IsExternalInit.cs
@@ -0,0 +1,19 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class IsExternalInit
+    {
+    }
+}
