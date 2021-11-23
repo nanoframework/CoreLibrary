d3a56f619463b7e22ebf67e8b25bd084e3016e0b
José Simões <jose.simoes@eclo.solutions>
2021-11-23 11:26:41 +0000

    Add RuntimeFeature.IsSupported
    - Check for DefaultImplementationsOfInterfaces.
    - Add unit tests for interfaces with default implementation.

---
+++ nanoFramework.CoreLibrary/System/Runtime/CompilerServices/RuntimeFeature.cs
@@ -0,0 +1,29 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// Portions Copyright (c) Microsoft Corporation.  All rights reserved.
+// See LICENSE file in the project root for full license information.
+//
+
+namespace System.Runtime.CompilerServices
+{
+    public static partial class RuntimeFeature
+    {
+        /// <summary>
+        /// Indicates that this version of runtime supports default interface method implementations.
+        /// </summary>
+        public const string DefaultImplementationsOfInterfaces = nameof(DefaultImplementationsOfInterfaces);
+
+        /// <summary>
+        /// Checks whether a certain feature is supported by the Runtime.
+        /// </summary>
+        public static bool IsSupported(string feature)
+        {
+            if (feature == DefaultImplementationsOfInterfaces)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
