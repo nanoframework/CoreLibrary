086ed3dd3f0ac9170b5153445cdad09555170e3c
josesimoes <jose.simoes@eclo.solutions>
2022-01-19 13:46:04 +0000

    Add TryParse
    - Implement method in the various classes.
    - Remove checks for null params in all Parse calls (this check is now carried in native).
    - Rework existing NativeToInt64 and NativeToDouble with param to set throwing exception and to return conversion success/failure.
    - Add Unit Tests and some rework on the existing ones where these were added.
    - Fix documentation and update formatting to follow code style.
    - Bump AssemblyNativeVersion 100.5.0.15.
    
    (temporarily disable float and double infinity tests)

--- nanoFramework.CoreLibrary/System/Byte.cs
+++ nanoFramework.CoreLibrary/System/Byte.cs
@@ -49,23 +49,41 @@ namespace System
         }
 
         /// <summary>
-        /// Converts the string representation of a number to its Byte equivalent.
+        /// Converts the string representation of a number to its <see cref="Byte"/> equivalent.
         /// </summary>
         /// <param name="s">A string that contains a number to convert. The string is interpreted using the Integer style. </param>
-        /// <returns>A byte value that is equivalent to the number contained in s.</returns>
-        /// <exception cref="System.ArgumentNullException">s is  null reference (Nothing in Visual Basic).</exception>
+        /// <returns>A <see cref="byte"/> value that is equivalent to the number contained in <paramref name="s"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="s"/> is <see langword="null"/>.</exception>
+        /// <exception cref="FormatException"><paramref name="s"/> is not of the correct format.</exception>
         [CLSCompliant(false)]
-        public static byte Parse(String s)
+        public static byte Parse(string s)
         {
-            if (s == null)
-            {
-#pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
-                throw new ArgumentNullException();
-#pragma warning restore S3928 // Parameter names used into ArgumentException constructors should match an existing one 
-            }
-
+            // check for null string is carried out in native code
             return Convert.ToByte(s);
         }
 
+        /// <summary>
+        /// Tries to convert the string representation of a number to its <see cref="Byte"/> equivalent, and returns a value that indicates whether the conversion succeeded.
+        /// </summary>
+        /// <param name="s">A <see cref="string"/> that contains a number to convert.</param>
+        /// <param name="result">When this method returns, contains the <see cref="byte"/> value equivalent to the number contained in <paramref name="s"/> if the conversion succeeded, or zero if the conversion failed. This parameter is passed uninitialized; any value originally supplied in <paramref name="result"/> will be overwritten.</param>
+        /// <returns>true if <paramref name="s"/> was converted successfully; otherwise, <see langword="false"/>.</returns>
+        /// <remarks>
+        /// The conversion fails and the method returns <see langword="false"/> if the s parameter is not in the correct format, if it is <see langword="null"/> or <see cref="string.Empty"/>, or if it represents a number less than <see cref="MinValue"/> or greater than <see cref="MaxValue"/>.
+        /// The <see cref="TryParse"/>(String, Byte) method is similar to the Byte.Parse(String) method, except that <see cref="TryParse"/>(String, Byte) does not throw an exception if the conversion fails.
+        /// </remarks>
+        public static bool TryParse(string s, out byte result)
+        {
+            result = (byte)Convert.NativeToInt64(
+                s,
+                true,
+                MinValue,
+                MaxValue,
+                10,
+                false,
+                out bool success);
+
+            return success;
+        }
     }
 }
