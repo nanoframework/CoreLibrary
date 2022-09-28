fda22101c2bfd1c03de92632470a5349a614a41f
José Simões <jose.simoes@eclo.solutions>
2022-09-28 14:01:12 +0100

    Improvements in ArrayList
    - Refactor code to use expression body for properties.
    - Replace sync root object with lazy init partern and a dedicated job instead of the class itself.

--- nanoFramework.CoreLibrary/System/Collections/ArrayList.cs
+++ nanoFramework.CoreLibrary/System/Collections/ArrayList.cs
@@ -18,8 +18,9 @@ namespace System.Collections
 #endif // NANOCLR_REFLECTION
     public class ArrayList : IList, ICloneable
     {
-        private Object[] _items;
+        private object[] _items;
         private int _size;
+        private object _syncRoot;
 
         // Keep in-sync with c_DefaultCapacity in CLR_RT_HeapBlock_ArrayList in NANOCLR_Runtime__HeapBlock.h
         private const int DefaultCapacity = 4;
@@ -29,7 +30,7 @@ namespace System.Collections
         /// </summary>
         public ArrayList()
         {
-            _items = new Object[DefaultCapacity];
+            _items = new object[DefaultCapacity];
         }
 
         /// <summary>
@@ -56,10 +57,7 @@ namespace System.Collections
         /// <value>
         /// The number of elements actually contained in the <see cref="ArrayList"/>.
         /// </value>
-        public virtual int Count
-        {
-            get { return _size; }
-        }
+        public virtual int Count => _size;
 
         /// <summary>
         /// Gets a value indicating whether the <see cref="ArrayList"/> has a fixed size.
@@ -67,10 +65,7 @@ namespace System.Collections
         /// <value>
         /// true if the <see cref="ArrayList"/> has a fixed size; otherwise, false. The default is false.
         /// </value>
-        public virtual bool IsFixedSize
-        {
-            get { return false; }
-        }
+        public virtual bool IsFixedSize => false;
 
         /// <summary>
         /// Gets a value indicating whether the <see cref="ArrayList"/> is read-only.
@@ -78,10 +73,7 @@ namespace System.Collections
         /// <value>
         /// true if the <see cref="ArrayList"/> is read-only; otherwise, false. The default is false.
         /// </value>
-        public virtual bool IsReadOnly
-        {
-            get { return false; }
-        }
+        public virtual bool IsReadOnly => false;
 
         /// <summary>
         /// Gets a value indicating whether access to the <see cref="ArrayList"/> is synchronized (thread safe).
@@ -89,10 +81,7 @@ namespace System.Collections
         /// <value>
         /// true if access to the <see cref="ArrayList"/> is synchronized (thread safe); otherwise, false. The default is false.
         /// </value>
-        public virtual bool IsSynchronized
-        {
-            get { return false; }
-        }
+        public virtual bool IsSynchronized => false;
 
         /// <summary>
         /// Gets an object that can be used to synchronize access to the <see cref="ArrayList"/>.
@@ -100,9 +89,14 @@ namespace System.Collections
         /// <value>
         /// An object that can be used to synchronize access to the <see cref="ArrayList"/>.
         /// </value>
-        public virtual Object SyncRoot
+        public virtual object SyncRoot
         {
-            get { return this; }
+            get
+            {
+                _syncRoot ??= new object();
+
+                return _syncRoot;
+            }
         }
 
         /// <summary>
@@ -110,7 +104,7 @@ namespace System.Collections
         /// </summary>
         /// <param name="index">The zero-based index of the element to get or set.</param>
         /// <returns></returns>
-        public virtual extern Object this[int index]
+        public virtual extern object this[int index]
         {
             [MethodImpl(MethodImplOptions.InternalCall)]
             get;
@@ -123,9 +117,9 @@ namespace System.Collections
         /// Adds an object to the end of the <see cref="ArrayList"/>.
         /// </summary>
         /// <param name="value"></param>
-        /// <returns>The <see cref="Object"/> to be added to the end of the <see cref="ArrayList"/>. The value can be <see langword="null"/>.</returns>
+        /// <returns>The <see cref="object"/> to be added to the end of the <see cref="ArrayList"/>. The value can be <see langword="null"/>.</returns>
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public virtual extern int Add(Object value);
+        public virtual extern int Add(object value);
 
         /// <summary>
         /// Searches the entire sorted <see cref="ArrayList"/> for an element using the specified comparer and returns the zero-based index of the element.
@@ -135,7 +129,7 @@ namespace System.Collections
         /// <para>-or-</para>
         /// <para>nullNothingnullptrunit a <see langword="null"/> reference(Nothing in Visual Basic) to use the IComparable implementation of each element.</para></param>
         /// <returns>The zero-based index of value in the sorted <see cref="ArrayList"/>, if value is found; otherwise, a negative number, which is the bitwise complement of the index of the next element that is larger than value or, if there is no larger element, the bitwise complement of Count.</returns>
-        public virtual int BinarySearch(Object value, IComparer comparer)
+        public virtual int BinarySearch(object value, IComparer comparer)
         {
             return Array.BinarySearch(_items, 0, _size, value, comparer);
         }
@@ -150,7 +144,7 @@ namespace System.Collections
         /// Creates a shallow copy of the <see cref="ArrayList"/>.
         /// </summary>
         /// <returns>A shallow copy of the <see cref="ArrayList"/>.</returns>
-        public virtual Object Clone()
+        public virtual object Clone()
         {
             var arrayList = new ArrayList();
 
@@ -158,7 +152,7 @@ namespace System.Collections
             {
                 // only re-allocate a new array if the size isn't what we need.
                 // otherwise, the one allocated in the constructor will be just fine
-                arrayList._items = new Object[_size];
+                arrayList._items = new object[_size];
             }
 
             arrayList._size = _size;
@@ -170,8 +164,8 @@ namespace System.Collections
         /// Determines whether an element is in the <see cref="ArrayList"/>.
         /// </summary>
         /// <param name="value"></param>
-        /// <returns>The<see cref="Object"/> to locate in the <see cref="ArrayList"/>.The value can be <see langword="null"/>.</returns>
-        public virtual bool Contains(Object value)
+        /// <returns>The<see cref="object"/> to locate in the <see cref="ArrayList"/>.The value can be <see langword="null"/>.</returns>
+        public virtual bool Contains(object value)
         {
             return Array.IndexOf(_items, value, 0, _size) >= 0;
         }
@@ -182,7 +176,9 @@ namespace System.Collections
         /// <param name="array">The one-dimensional Array that is the destination of the elements copied from <see cref="ArrayList"/>. The Array must have zero-based indexing.</param>
         public virtual void CopyTo(Array array)
         {
-            CopyTo(array, 0);
+            CopyTo(
+                array,
+                0);
         }
 
         /// <summary>
@@ -190,9 +186,16 @@ namespace System.Collections
         /// </summary>
         /// <param name="array">The one-dimensional Array that is the destination of the elements copied from <see cref="ArrayList"/>. The Array must have zero-based indexing. </param>
         /// <param name="index">The zero-based index in array at which copying begins. </param>
-        public virtual void CopyTo(Array array, int index)
+        public virtual void CopyTo(
+            Array array,
+            int index)
         {
-            Array.Copy(_items, 0, array, index, _size);
+            Array.Copy(
+                _items,
+                0,
+                array,
+                index,
+                _size);
         }
 
         /// <summary>
@@ -201,7 +204,10 @@ namespace System.Collections
         /// <returns>An IEnumerator for the entire <see cref="ArrayList"/>.</returns>
         public virtual IEnumerator GetEnumerator()
         {
-            return new Array.SzArrayEnumerator(_items, 0, _size);
+            return new Array.SzArrayEnumerator(
+                _items,
+                0,
+                _size);
         }
 
         /// <summary>
@@ -209,9 +215,13 @@ namespace System.Collections
         /// </summary>
         /// <param name="value">The Object to locate in the <see cref="ArrayList"/>. The value can be <see langword="null"/> reference (Nothing in Visual Basic). </param>
         /// <returns>The zero-based index of the first occurrence of value within the entire <see cref="ArrayList"/>, if found; otherwise, -1.</returns>
-        public virtual int IndexOf(Object value)
+        public virtual int IndexOf(object value)
         {
-            return Array.IndexOf(_items, value, 0, _size);
+            return Array.IndexOf(
+                _items,
+                value,
+                0,
+                _size);
         }
 
         /// <summary>
@@ -220,9 +230,13 @@ namespace System.Collections
         /// <param name="value">The Object to locate in the <see cref="ArrayList"/>. The value can be <see langword="null"/> reference (Nothing in Visual Basic). </param>
         /// <param name="startIndex">The zero-based starting index of the search. 0 (zero) is valid in an empty list.</param>
         /// <returns>The zero-based index of the first occurrence of value within the range of elements in the <see cref="ArrayList"/> that extends from startIndex to the last element, if found; otherwise, -1.</returns>
-        public virtual int IndexOf(Object value, int startIndex)
+        public virtual int IndexOf(object value, int startIndex)
         {
-            return Array.IndexOf(_items, value, startIndex, _size - startIndex);
+            return Array.IndexOf(
+                _items,
+                value,
+                startIndex,
+                _size - startIndex);
         }
 
         /// <summary>
@@ -232,26 +246,37 @@ namespace System.Collections
         /// <param name="startIndex">The zero-based starting index of the search. 0 (zero) is valid in an empty list.</param>
         /// <param name="count">The number of elements in the section to search. </param>
         /// <returns>The zero-based index of the first occurrence of value within the range of elements in the <see cref="ArrayList"/> that starts at startIndex and contains count number of elements, if found; otherwise, -1.</returns>
-        public virtual int IndexOf(Object value, int startIndex, int count)
+        public virtual int IndexOf(object value, int startIndex, int count)
         {
-            return Array.IndexOf(_items, value, startIndex, count);
+            return Array.IndexOf(
+                _items,
+                value,
+                startIndex,
+                count);
         }
 
         /// <summary>
         /// Inserts an element into the <see cref="ArrayList"/> at the specified index.
         /// </summary>
         /// <param name="index">The zero-based index at which <para>value</para> should be inserted.</param>
-        /// <param name="value">The <see cref="Object"/> to insert. The `value` can be <see langword="null"/>.</param>
+        /// <param name="value">The <see cref="object"/> to insert. The `value` can be <see langword="null"/>.</param>
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public virtual extern void Insert(int index, Object value);
+        public virtual extern void Insert(
+            int index,
+            object value);
 
         /// <summary>
         /// Removes the first occurrence of a specific object from the <see cref="ArrayList"/>.
         /// </summary>
-        /// <param name="value">The <see cref="Object"/> to remove from the <see cref="ArrayList"/>. The value can be <see langword="null"/>.</param>
-        public virtual void Remove(Object value)
+        /// <param name="value">The <see cref="object"/> to remove from the <see cref="ArrayList"/>. The value can be <see langword="null"/>.</param>
+        public virtual void Remove(object value)
         {
-            var index = Array.IndexOf(_items, value, 0, _size);
+            var index = Array.IndexOf(
+                _items,
+                value,
+                0,
+                _size);
+
             if (index >= 0)
             {
                 RemoveAt(index);
@@ -268,13 +293,13 @@ namespace System.Collections
 #if NANOCLR_REFLECTION
 
         /// <summary>
-        /// Copies the elements of the <see cref="ArrayList"/> to a new <see cref="Object"/> array.
+        /// Copies the elements of the <see cref="ArrayList"/> to a new <see cref="object"/> array.
         /// </summary>
         /// <returns>An Object array containing copies of the elements of the <see cref="ArrayList"/>.</returns>
         /// <remarks>Available only in mscorlib build with support for System.Reflection.</remarks>
-        public virtual Object[] ToArray()
+        public virtual object[] ToArray()
         {
-            return (Object[])ToArray(typeof(object));
+            return (object[])ToArray(typeof(object));
         }
 
         /// <summary>
