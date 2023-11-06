cc73a17eea409766ba2bc27d990c5a56de6a723f
Cory Charlton <ccharlton@pobox.com>
2023-11-06 13:40:09 -0800

    Switching min/max to managed implementations

--- nanoFramework.CoreLibrary/System/Math.cs
+++ nanoFramework.CoreLibrary/System/Math.cs
@@ -13,10 +13,26 @@ namespace System
         [MethodImpl(MethodImplOptions.InternalCall)]
         internal static extern int Abs(int val);
 
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        internal static extern int Min(int val1, int val2);
+        /// <summary>
+        /// Returns the larger of two 32-bit signed integers.
+        /// </summary>
+        /// <param name="val1">The first of two 32-bit signed integers to compare. </param>
+        /// <param name="val2">The second of two 32-bit signed integers to compare. </param>
+        /// <returns>Parameter <paramref name="val1"/> or <paramref name="val2"/>, whichever is larger.</returns>
+        internal static int Max(int val1, int val2)
+        {
+            return (val1 >= val2) ? val1 : val2;
+        }
 
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        internal static extern int Max(int val1, int val2);
+        /// <summary>
+        /// Returns the smaller of two 32-bit signed integers.
+        /// </summary>
+        /// <param name="val1">The first of two 32-bit signed integers to compare. </param>
+        /// <param name="val2">The second of two 32-bit signed integers to compare. </param>
+        /// <returns>Parameter <paramref name="val1"/> or <paramref name="val2"/>, whichever is smaller.</returns>
+        internal static int Min(int val1, int val2)
+        {
+            return (val2 >= val1) ? val1 : val2;
+        }
     }
 }
