8060677255f1c65afdd321c2dd150fdc2a975f5b
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-01-20 10:03:09 -0800

    Moving attributes from nanoFramework.System.Runtime

---
+++ nanoFramework.CoreLibrary/System/Runtime/CompilerServices/CallerMemberNameAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Allows you to obtain the method or property name of the caller to the method.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    public sealed class CallerMemberNameAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallerMemberNameAttribute"/> class.
+        /// </summary>
+        public CallerMemberNameAttribute()
+        {
+        }
+    }
+}
