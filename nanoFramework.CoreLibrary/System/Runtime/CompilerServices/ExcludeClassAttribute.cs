fe64d61e84c92bc012e920b896c669a703061f4b
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-01-28 13:27:52 -0800

    Add ExcludeClassAttribute

---
+++ nanoFramework.CoreLibrary/System/Runtime/CompilerServices/ExcludeClassAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// When a class is marked with <see cref="ExcludeClassAttribute"/> it will be excluded from the native stubs generated for the nanoFramework interpreter.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct, AllowMultiple = false, Inherited = true)]
+    public class ExcludeClassAttribute: Attribute
+    {
+    }
+}
