c33f1eabd562d3370f44d6e70c301b9200f01da2
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-01-29 04:43:43 -0800

    Moving attributes from System.Runtime (#219)
    ***NO_CI***
    
    (cherry picked from commit db43573874ce6d531c2b4f33f9a4445a71f89498)

---
+++ Tests/NFUnitTestAttributes/CallerArgumentExpressionAttributeTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+using nanoFramework.TestFramework;
+
+namespace NFUnitTestAttributes
+{
+    [TestClass]
+    public class CallerArgumentExpressionAttributeTests
+    {
+        public string Field = "Field value";
+
+        public string Property => "Property value";
+
+        [TestMethod]
+        public void CallerArgumentExpressionSetsParameterNameFromField()
+        {
+            var sut = new CallerArgumentExpressionAttributeTests();
+            const string expect = "sut.Field";
+            var actual = TestCallerArgumentExpression(sut.Field);
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void CallerArgumentExpressionSetsParameterNameFromMethodParameter()
+        {
+            const string expect = "methodParameter";
+            var actual = TestMethodParameter("Method parameter value");
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void CallerArgumentExpressionSetsParameterNameFromNull()
+        {
+            const string expect = "null";
+            var actual = TestCallerArgumentExpression(null);
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void CallerArgumentExpressionSetsParameterNameFromProperty()
+        {
+            var sut = new CallerArgumentExpressionAttributeTests();
+            const string expect = "sut.Property";
+            var actual = TestCallerArgumentExpression(sut.Property);
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void CallerArgumentExpressionSetsParameterNameFromVariable()
+        {
+            const string variableName = "Variable value";
+            const string expect = nameof(variableName);
+            var actual = TestCallerArgumentExpression(variableName);
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        // ReSharper disable once EntityNameCapturedOnly.Local
+#pragma warning disable IDE0060
+        private static string TestCallerArgumentExpression(object objectValue, [CallerArgumentExpression(nameof(objectValue))] string parameterName = null) => parameterName;
+#pragma warning restore IDE0060
+
+        private static string TestMethodParameter(string methodParameter) => TestCallerArgumentExpression(methodParameter);
+    }
+}
---
+++ Tests/NFUnitTestAttributes/CallerMemberNameAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using nanoFramework.TestFramework;
+
+namespace NFUnitTestAttributes
+{
+    [TestClass]
+    public class CallerMemberNameAttributeTests
+    {
+        [TestMethod]
+        public void CallerMemberNameAttributeGetsCallerMemberName()
+        {
+            const string expect = nameof(CallerMemberNameAttributeGetsCallerMemberName);
+            var actual = TestCallerMemberName();
+
+            Assert.AreEqual(expect, actual);
+        }
+
+        // ReSharper disable once EntityNameCapturedOnly.Local
+#pragma warning disable IDE0060
+        private static string TestCallerMemberName([CallerMemberName] string memberName = null) => memberName;
+#pragma warning restore IDE0060
+    }
+}
