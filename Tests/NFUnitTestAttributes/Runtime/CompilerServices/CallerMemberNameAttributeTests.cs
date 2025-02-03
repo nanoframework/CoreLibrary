475ca13e4bc84c94ff235596735ecc083926a63a
Cory Charlton <CoryCharlton@users.noreply.github.com>
2025-02-03 15:02:57 -0800

    Add unit tests for new nullable attributes (#225)

--- Tests/NFUnitTestAttributes/Runtime/CompilerServices/CallerMemberNameAttributeTests.cs
+++ Tests/NFUnitTestAttributes/Runtime/CompilerServices/CallerMemberNameAttributeTests.cs
@@ -4,7 +4,7 @@
 using System.Runtime.CompilerServices;
 using nanoFramework.TestFramework;
 
-namespace NFUnitTestAttributes
+namespace NFUnitTestAttributes.Runtime.CompilerServices
 {
     [TestClass]
     public class CallerMemberNameAttributeTests
