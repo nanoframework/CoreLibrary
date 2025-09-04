c525e7aa37d391e9d1a0d6165299160821ea9011
José Simões <jose.simoes@eclo.solutions>
2025-09-04 11:13:02 +0100

    Fix IntelliSense comments in ReadOnlySpan<T>

--- nanoFramework.CoreLibrary/System/ReadOnlySpan.cs
+++ nanoFramework.CoreLibrary/System/ReadOnlySpan.cs
@@ -4,13 +4,14 @@
 using System.Diagnostics;
 
 #pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
+#nullable enable
 
 namespace System
 {
     /// <summary>
-    /// <see cref="ReadOnlySpan"/> represents a contiguous region of arbitrary memory. Unlike arrays, it can point to either managed
-    /// or native memory, or to memory allocated on the stack. It is type-safe and memory-safe.
+    /// Provides a type-safe and memory-safe read-only representation of a contiguous region of arbitrary memory.
     /// </summary>
+    /// <typeparam name="T">The type of items in the <see cref="ReadOnlySpan{T}"/>.</typeparam>
     [DebuggerTypeProxy(typeof(SpanDebugView<>))]
     [DebuggerDisplay("{ToString(),raw}")]
     public readonly ref struct ReadOnlySpan<T>
@@ -20,10 +21,10 @@ namespace System
         private readonly int _length;
 
         /// <summary>
-        /// Creates a new read-only span over the entirety of the target array.
+        /// Creates a new <see cref="ReadOnlySpan{T}"/> over the entirety of a specified array.
         /// </summary>
-        /// <param name="array">The target array.</param>
-        /// <remarks>Returns default when <paramref name="array"/> is null.</remarks>
+        /// <param name="array">The array from which to create the <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <remarks>If the array is <see langword="null"/>, this constructor returns a null <see cref="ReadOnlySpan{T}"/>.</remarks>
         public ReadOnlySpan(T[]? array)
         {
             _array = array ?? Array.Empty<T>();
@@ -32,15 +33,29 @@ namespace System
         }
 
         /// <summary>
-        /// Creates a new read-only span over the portion of the target array beginning
-        /// at 'start' index and ending at 'end' index (exclusive).
+        /// Creates a new <see cref="ReadOnlySpan{T}"/> that includes a specified number of elements of an array starting at a specified index.
         /// </summary>
-        /// <param name="array">The target array.</param>
-        /// <param name="start">The zero-based index at which to begin the read-only span.</param>
-        /// <param name="length">The number of items in the read-only span.</param>
+        /// <param name="array">The array from which to create the <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <param name="start">The zero-based index of the first element to include.</param>
+        /// <param name="length">The number of elements to include.</param>
+        /// <returns>This method returns <see langword="default"/> when <paramref name="array"/> is <see langword="null"/>.</returns>
         /// <remarks>Returns default when <paramref name="array"/> is null.</remarks>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// Thrown when the specified <paramref name="start"/> or end index is not in the range (&lt;0 or &gt;Length).
+        /// <para>
+        /// <paramref name="array"/> is <see langword="null"/>, but <paramref name="start"/> or <paramref name="length"/> is non-zero.
+        /// </para>
+        /// <para>
+        /// -or-
+        /// </para>
+        /// <para>
+        /// <paramref name="start"/> is outside the bounds of the array.
+        /// </para>
+        /// <para>
+        /// -or-
+        /// </para>
+        /// <para>
+        /// <paramref name="start"/> and <paramref name="length"/> exceeds the number of elements in the array.
+        /// </para>
         /// </exception>
         public ReadOnlySpan(T[]? array, int start, int length)
         {
@@ -70,7 +85,7 @@ namespace System
         }
 
         /// <summary>
-        /// Returns the specified element of the read-only span.
+        /// Returns the specified element of the <see cref="ReadOnlySpan{T}"/>.
         /// </summary>
         /// <param name="index">The zero-based index.</param>
         /// <returns></returns>
@@ -96,15 +111,22 @@ namespace System
         public int Length => _length;
 
         /// <summary>
-        /// Gets a value indicating whether this <see cref="Span{T}"/> is empty.
+        /// Returns a value that indicates the current read-only span is empty.
         /// </summary>
-        /// <value><see langword="true"/> if this span is empty; otherwise, <see langword="false"/>.</value>
+        /// <value><see langword="true"/> when the current span is empty; otherwise, <see langword="false"/>.</value>
         public bool IsEmpty => _length == 0;
 
         /// <summary>
-        /// Returns false if left and right point at the same memory and have the same length.  Note that
-        /// this does *not* check to see if the *contents* are equal.
+        /// Returns a value that indicates whether two <see cref="ReadOnlySpan{T}"/> instances are not equal.
         /// </summary>
+        /// <param name="left">The first read-only span to compare.</param>
+        /// <param name="right">The second read-only span to compare.</param>
+        /// <returns>
+        /// <see langword="true"/> if the two <see cref="ReadOnlySpan{T}"/> instances are not equal; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// Two <see cref="ReadOnlySpan{T}"/> instances are not equal if they have different lengths or if the corresponding elements of <paramref name="left"/> and <paramref name="right"/> point to different memory locations.
+        /// </remarks>
         public static bool operator !=(ReadOnlySpan<T> left, ReadOnlySpan<T> right) => !(left == right);
 
         /// <summary>
@@ -113,9 +135,16 @@ namespace System
         public static implicit operator ReadOnlySpan<T>(T[]? array) => new ReadOnlySpan<T>(array);
 
         /// <summary>
-        /// Returns true if left and right point at the same memory and have the same length.  Note that
-        /// this does *not* check to see if the *contents* are equal.
+        /// Returns a value that indicates whether two <see cref="ReadOnlySpan{T}"/> instances are equal.
         /// </summary>
+        /// <param name="left">The first span to compare.</param>
+        /// <param name="right">The second span to compare.</param>
+        /// <returns>
+        /// <see langword="true"/> if the two <see cref="ReadOnlySpan{T}"/> instances are equal; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// This tests if two <see cref="ReadOnlySpan{T}"/> instances are equal by comparing their lengths and the elements they point to. Two <see cref="ReadOnlySpan{T}"/> instances point to the same starting memory location, and have the same <see cref="Length"/> values. This does not compare the contents of two <see cref="ReadOnlySpan{T}"/> instances.
+        /// </remarks>
         public static bool operator ==(ReadOnlySpan<T> left, ReadOnlySpan<T> right)
         {
             if (left.Length != right.Length)
@@ -135,10 +164,14 @@ namespace System
         }
 
         /// <summary>
-        /// Copies the contents of this read-only span into a new array.  This heap
-        /// allocates, so should generally be avoided, however it is sometimes
-        /// necessary to bridge the gap with APIs written in terms of arrays.
+        /// Copies the contents of this read-only span into a new array.
         /// </summary>
+        /// <returns>
+        /// An array containing the data in the current span.
+        /// </returns>
+        /// <remarks>
+        /// This method performs a heap allocation and data copy, and therefore, should be avoided if possible. It it sometimes necessary when APIs that support <see cref="ReadOnlySpan{T}"/> are not available, but APIs that work with arrays are.
+        /// </remarks>
         public T[] ToArray()
         {
             T[] destination = new T[_length];
