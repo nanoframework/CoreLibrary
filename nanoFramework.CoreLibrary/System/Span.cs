c6632c85a4f66affbde7c847ded220a2212981fb
José Simões <jose.simoes@eclo.solutions>
2025-09-04 11:14:04 +0100

    Fixes in Span<T>
    - Add missing null check in constructor.
    - Fix IntelliSense comments.

--- nanoFramework.CoreLibrary/System/Span.cs
+++ nanoFramework.CoreLibrary/System/Span.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 #pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
+#nullable enable
 
 namespace System
 {
@@ -12,6 +13,7 @@ namespace System
     /// Span represents a contiguous region of arbitrary memory. Unlike arrays, it can point to either managed
     /// or native memory, or to memory allocated on the stack. It is type-safe and memory-safe.
     /// </summary>
+    /// <typeparam name="T">The type of items in the <see cref="Span{T}"/>.</typeparam>
     [DebuggerTypeProxy(typeof(SpanDebugView<>))]
     [DebuggerDisplay("{ToString(),raw}")]
     public readonly ref struct Span<T>
@@ -21,34 +23,55 @@ namespace System
         private readonly int _length;
 
         /// <summary>
-        /// Creates a new span over the entirety of the target array.
+        /// <summary>
+        /// Creates a new <see cref="Span{T}"/> object over the entirety of a specified array.
         /// </summary>
-        /// <param name="array">The target array.</param>
-        /// <remarks>Returns default when <paramref name="array"/> is null.</remarks>
-        /// <exception cref="ArrayTypeMismatchException">Thrown when <paramref name="array"/> is covariant and array's type is not exactly T[].</exception>
+        /// </summary>
+        /// <param name="array">The array from which to create the <see cref="Span{T}"/> object.</param>
+        /// <remarks>If array is <see langword="null"/>, this constructor returns a <see langword="null"/> <see cref="Span{T}"/>.</remarks>
+        /// <exception cref="ArrayTypeMismatchException"><see typeparamref="T"/> is a reference type, and <paramref name="array"/> is not an array of type <see typeparamref ="T"/>.</exception>
         public Span(T[]? array)
         {
+            if (array == null)
+            {
+                this = default;
+                return;
+            }
+
             if (!typeof(T).IsValueType && array.GetType() != typeof(T[]))
             {
                 throw new ArrayTypeMismatchException();
             }
 
-            _array = array ?? Array.Empty<T>();
+            _array = array ?? [];
             _start = 0;
             _length = _array.Length;
         }
 
         /// <summary>
-        /// Creates a new span over the portion of the target array beginning
-        /// at 'start' index and ending at 'end' index (exclusive).
+        /// Creates a new <see cref="Span{T}"/> object that includes a specified number of elements of an array starting at a specified index.
         /// </summary>
-        /// <param name="array">The target array.</param>
-        /// <param name="start">The zero-based index at which to begin the span.</param>
-        /// <param name="length">The number of items in the span.</param>
-        /// <remarks>Returns default when <paramref name="array"/> is null.</remarks>
-        /// <exception cref="ArrayTypeMismatchException">Thrown when <paramref name="array"/> is covariant and array's type is not exactly T[].</exception>
+        /// <param name="array">The source array.</param>
+        /// <param name="start">The zero-based index of the first element to include in the new <see cref="Span{T}"/>.</param>
+        /// <param name="length">The number of elements to include in the new <see cref="Span{T}"/>.</param>
+        /// <remarks>This method returns <see langword="default"/> when <paramref name="array"/> is <see langword="null"/>.</remarks>
+        /// <exception cref="ArrayTypeMismatchException"><see typeparamref="T"/> is a reference type, and <paramref name="array"/> is not an array of type <see typeparamref="T"/>.</exception>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// Thrown when the specified <paramref name="start"/> or end index is not in the range (&lt;0 or &gt;Length).
+        /// <para>
+        /// <paramref name="array"/> is <see langword="null"/>, but <paramref name="start"/> or <paramref name="length"/> is non-zero.
+        /// </para>
+        /// <para>
+        /// -or-
+        /// </para>
+        /// <para>
+        /// <paramref name="start"/> is outside the bounds of the array.
+        /// </para>
+        /// <para>
+        /// -or-
+        /// </para>
+        /// <para>
+        /// <paramref name="start"/> and <paramref name="length"/> exceeds the number of elements in the array.
+        /// </para>
         /// </exception>
         public Span(
             T[]? array,
@@ -105,12 +128,12 @@ namespace System
         }
 
         /// <summary>
-        /// Returns a reference to specified element of the Span.
+        /// Returns a reference to specified element of the span.
         /// </summary>
         /// <param name="index">The zero-based index.</param>
         /// <returns></returns>
         /// <exception cref="IndexOutOfRangeException">
-        /// Thrown when index less than 0 or index greater than or equal to Length
+        /// Thrown when <paramref name="index"/> less than 0 or <paramref name="index"/> greater than or equal to <see cref="Length"/>.
         /// </exception>
         public ref T this[int index]
         {
@@ -126,31 +149,41 @@ namespace System
         }
 
         /// <summary>
-        /// The number of items in the span.
+        /// Returns the length of the current span.
         /// </summary>
         public int Length => _length;
 
         /// <summary>
-        /// Gets a value indicating whether this <see cref="Span{T}"/> is empty.
+        /// Returns a value that indicates whether the current <see cref="Span{T}"/> is empty.
         /// </summary>
         /// <value><see langword="true"/> if this span is empty; otherwise, <see langword="false"/>.</value>
         public bool IsEmpty => _length == 0;
 
         /// <summary>
-        /// Returns false if left and right point at the same memory and have the same length.  Note that
-        /// this does *not* check to see if the *contents* are equal.
+        /// Returns a value that indicates whether two <see cref="Span{T}"/> objects are not equal.
         /// </summary>
+        /// <param name="left">The first span to compare.</param>
+        /// <param name="right">The second span to compare.</param>
+        /// <returns>
+        /// <see langword="true"/> if the two <see cref="Span{T}"/> objects are not equal; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// Two <see cref="Span{T}"/> objects are are not equal if they have different lengths or if the corresponding elements of <paramref name="left"/> and <paramref name="right"/> do not point to the same memory.
+        /// </remarks>
         public static bool operator !=(Span<T> left, Span<T> right) => !(left == right);
 
         /// <summary>
-        /// Returns an empty <see cref="Span{T}"/>
+        /// Returns an empty <see cref="Span{T}"/> object.
         /// </summary>
         public static Span<T> Empty => default;
 
-        /// <summary>Gets an enumerator for this span.</summary>
+        /// <summary>Gets an enumerator for this <see cref="Span{T}"/>.</summary>
+        /// <returns>An enumerator for this span.</returns>
         public Enumerator GetEnumerator() => new Enumerator(this);
 
-        /// <summary>Enumerates the elements of a <see cref="Span{T}"/>.</summary>
+        /// <summary>
+        /// Provides an enumerator for the elements of a <see cref="Span{T}"/>.
+        /// </summary>
         public ref struct Enumerator
         {
             /// <summary>The span being enumerated.</summary>
@@ -166,10 +199,14 @@ namespace System
                 _index = -1;
             }
 
-            /// <summary>Advances the enumerator to the next element of the span.</summary>
+            /// <summary>Advances the enumerator to the next item of the <see cref="Span{T}"/>.</summary>
+            /// <returns>
+            /// <see langword="true"/> if the enumerator successfully advanced to the next item; <see langword="false"/> if the end of the span has been passed.
+            /// </returns>
             public bool MoveNext()
             {
                 int index = _index + 1;
+
                 if (index < _span.Length)
                 {
                     _index = index;
@@ -179,13 +216,16 @@ namespace System
                 return false;
             }
 
-            /// <summary>Gets the element at the current position of the enumerator.</summary>
+            /// <summary>Gets a reference to the item at the current position of the enumerator.</summary>
             public ref T Current => ref _span[_index];
         }
 
         /// <summary>
-        /// Clears the contents of this span.
+        /// Clears the contents of this <see cref="Span{T}"/> object.
         /// </summary>
+        /// <remarks>
+        /// The <see cref="Clear"/> method sets the items in the <see cref="Span{T}"/> object to their default values. It does not remove items from the <see cref="Span{T}"/>.
+        /// </remarks>
         public void Clear()
         {
             Array.Clear(
@@ -195,36 +235,50 @@ namespace System
         }
 
         /// <summary>
-        /// Copies the contents of this span into destination span. If the source
-        /// and destinations overlap, this method behaves as if the original values in
-        /// a temporary location before the destination is overwritten.
+        /// Copies the contents of this <see cref="Span{T}"/> into a destination <see cref="Span{T}"/>.
         /// </summary>
-        /// <param name="destination">The span to copy items into.</param>
+        /// <param name="destination">The destination <see cref="Span{T}"/> object.</param>
         /// <exception cref="ArgumentException">
-        /// Thrown when the destination Span is shorter than the source Span.
+        /// <paramref name="destination"/> is shorter than the source <see cref="Span{T}"/>.
         /// </exception>
+        /// <remarks>
+        /// This method copies all of source to <paramref name="destination"/> even if source and <paramref name="destination"/> overlap.
+        /// </remarks>
         [MethodImpl(MethodImplOptions.InternalCall)]
         public extern void CopyTo(Span<T> destination);
 
         /// <summary>
-        /// Returns true if left and right point at the same memory and have the same length.  Note that
-        /// this does *not* check to see if the *contents* are equal.
+        /// Returns a value that indicates whether two <see cref="Span{T}"/> objects are equal.
         /// </summary>
+        /// <param name="left">The first span to compare.</param>
+        /// <param name="right">The second span to compare.</param>
+        /// <returns>
+        /// <see langword="true"/> if the two <see cref="Span{T}"/> objects are equal; otherwise, <see langword="false"/>.
+        /// </returns>
+        /// <remarks>
+        /// Two <see cref="Span{T}"/> objects are equal if they have the same length and if the corresponding elements of <paramref name="left"/> and <paramref name="right"/> point to the same memory.
+        /// Note that the test for equality does *not* attempt to determine if the contents are equal.
+        /// </remarks>
         public static bool operator ==(Span<T> left, Span<T> right) =>
            left._length == right._length && left._array == right._array && left._start == right._start;
 
         /// <summary>
         /// Defines an implicit conversion of a <see cref="Span{T}"/> to a <see cref="ReadOnlySpan{T}"/>
         /// </summary>
+        /// <param name="span">The object to convert to a <see cref="ReadOnlySpan{T}"/>.</param>
+        /// <returns>
+        /// A read-only span that corresponds to the current instance.
+        /// </returns>
         public static implicit operator ReadOnlySpan<T>(Span<T> span) =>
              new ReadOnlySpan<T>(span._array, span._start, span._length);
 
         /// <summary>
-        /// Forms a slice out of the given span, beginning at 'start'.
+        /// Forms a slice out of the current span that begins at a specified index.
         /// </summary>
-        /// <param name="start">The zero-based index at which to begin this slice.</param>
+        /// <param name="start">The zero-based index at which to begin the slice.</param>
+        /// <returns>A span that consists of all elements of the current span from <paramref name="start"/> to the end of the span.</returns>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// Thrown when the specified <paramref name="start"/> index is not in range (&lt;0 or &gt;Length).
+        /// <paramref name="start"/> is less than zero or greater than <see cref="Length"/>.
         /// </exception>
         public Span<T> Slice(int start)
         {
@@ -237,12 +291,13 @@ namespace System
         }
 
         /// <summary>
-        /// Forms a slice out of the given span, beginning at 'start', of given length
+        /// Forms a slice out of the current span starting at a specified index for a specified length.
         /// </summary>
-        /// <param name="start">The zero-based index at which to begin this slice.</param>
-        /// <param name="length">The desired length for the slice (exclusive).</param>
+        /// <param name="start">The zero-based index at which to begin the slice.</param>
+        /// <param name="length">The desired length for the slice.</param>
+        /// <returns>A span that consists of <paramref name="length"/> elements from the current span starting at <paramref name="start"/>.</returns>
         /// <exception cref="ArgumentOutOfRangeException">
-        /// Thrown when the specified <paramref name="start"/> or end index is not in range (&lt;0 or &gt;Length).
+        /// <paramref name="start"/> or <paramref name="start"/> + <paramref name="length"/> is less than zero or greater than <see cref="Length"/>.
         /// </exception>
         public Span<T> Slice(int start, int length)
         {
@@ -256,10 +311,14 @@ namespace System
         }
 
         /// <summary>
-        /// Copies the contents of this span into a new array.  This heap
-        /// allocates, so should generally be avoided, however it is sometimes
-        /// necessary to bridge the gap with APIs written in terms of arrays.
+        /// Copies the contents of this span into a new array.
         /// </summary>
+        /// <returns>
+        /// An array containing the data in the current span.
+        /// </returns>
+        /// <remarks>
+        /// This method performs a heap allocation and therefore should be avoided if possible. Heap allocations are expected in APIs that work with arrays. Using such APIs is unavoidable if an alternative API overload that takes a <see cref="Span{T}"/> does not exist.
+        /// </remarks>
         public T[] ToArray()
         {
             T[] destination = new T[_length];
