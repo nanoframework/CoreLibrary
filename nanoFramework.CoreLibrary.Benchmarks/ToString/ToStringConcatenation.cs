51be4705b051ed70dc31143b44c285f1b8d5f120
José Simões <jose.simoes@eclo.solutions>
2025-11-24 11:14:02 +0000

    Add benchmark tests for ToString
    (cherry picked from commit 81808e5f8c03441fe16b24b75f0d85f08c0386c2)

---
+++ nanoFramework.CoreLibrary.Benchmarks/ToString/ToStringConcatenation.cs
@@ -0,0 +1,99 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using nanoFramework.Benchmark;
+using nanoFramework.Benchmark.Attributes;
+
+namespace nanoFramework.CoreLibrary.Benchmarks.ToString
+{
+    [IterationCount(5)]
+    public class ToStringConcatenation
+    {
+        private int _intValue;
+        private long _longValue;
+        private double _doubleValue;
+        private bool _boolValue;
+        private byte _byteValue;
+        private DateTime _dateTimeValue;
+        private string _stringValue;
+
+        [Setup]
+        public void Setup()
+        {
+            _intValue = 12345;
+            _longValue = 1234567890123456L;
+            _doubleValue = 123.456;
+            _boolValue = true;
+            _byteValue = 255;
+            _dateTimeValue = new DateTime(2024, 1, 15, 10, 30, 45);
+            _stringValue = "test";
+        }
+
+        [Benchmark]
+        public void Int32Concatenation()
+        {
+            _ = "Value: " + _intValue;
+        }
+
+        [Benchmark]
+        public void Int64Concatenation()
+        {
+            _ = "Value: " + _longValue;
+        }
+
+        [Benchmark]
+        public void DoubleConcatenation()
+        {
+            _ = "Value: " + _doubleValue;
+        }
+
+        [Benchmark]
+        public void BooleanConcatenation()
+        {
+            _ = "Value: " + _boolValue;
+        }
+
+        [Benchmark]
+        public void ByteConcatenation()
+        {
+            _ = "Value: " + _byteValue;
+        }
+
+        [Benchmark]
+        public void DateTimeConcatenation()
+        {
+            _ = "Value: " + _dateTimeValue;
+        }
+
+        [Benchmark]
+        public void MultipleValuesConcatenation()
+        {
+            _ = "Int: " + _intValue + ", Double: " + _doubleValue + ", Bool: " + _boolValue;
+        }
+
+        [Benchmark]
+        public void MixedConcatenation()
+        {
+            _ = "String: " + _stringValue + ", Int: " + _intValue + ", DateTime: " + _dateTimeValue;
+        }
+
+        [Benchmark]
+        public void ComplexConcatenation()
+        {
+            _ = "Values - Byte: " + _byteValue + ", Long: " + _longValue + ", Double: " + _doubleValue + ", Bool: " + _boolValue;
+        }
+
+        [Benchmark]
+        public void ExplicitToStringConcatenation()
+        {
+            _ = "Value: " + _intValue.ToString();
+        }
+
+        [Benchmark]
+        public void MultipleExplicitToStringConcatenation()
+        {
+            _ = "Int: " + _intValue.ToString() + ", Double: " + _doubleValue.ToString() + ", Bool: " + _boolValue.ToString();
+        }
+    }
+}
---
+++ nanoFramework.CoreLibrary.Benchmarks/ToString/ToStringInterpolation.cs
@@ -0,0 +1,87 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using nanoFramework.Benchmark;
+using nanoFramework.Benchmark.Attributes;
+
+namespace nanoFramework.CoreLibrary.Benchmarks.ToString
+{
+    [IterationCount(5)]
+    public class ToStringInterpolation
+    {
+        private int _intValue;
+        private long _longValue;
+        private double _doubleValue;
+        private bool _boolValue;
+        private byte _byteValue;
+        private DateTime _dateTimeValue;
+        private string _stringValue;
+
+        [Setup]
+        public void Setup()
+        {
+            _intValue = 12345;
+            _longValue = 1234567890123456L;
+            _doubleValue = 123.456;
+            _boolValue = true;
+            _byteValue = 255;
+            _dateTimeValue = new DateTime(2024, 1, 15, 10, 30, 45);
+            _stringValue = "test";
+        }
+
+        [Benchmark]
+        public void Int32Interpolation()
+        {
+            _ = $"Value: {_intValue}";
+        }
+
+        [Benchmark]
+        public void Int64Interpolation()
+        {
+            _ = $"Value: {_longValue}";
+        }
+
+        [Benchmark]
+        public void DoubleInterpolation()
+        {
+            _ = $"Value: {_doubleValue}";
+        }
+
+        [Benchmark]
+        public void BooleanInterpolation()
+        {
+            _ = $"Value: {_boolValue}";
+        }
+
+        [Benchmark]
+        public void ByteInterpolation()
+        {
+            _ = $"Value: {_byteValue}";
+        }
+
+        [Benchmark]
+        public void DateTimeInterpolation()
+        {
+            _ = $"Value: {_dateTimeValue}";
+        }
+
+        [Benchmark]
+        public void MultipleValuesInterpolation()
+        {
+            _ = $"Int: {_intValue}, Double: {_doubleValue}, Bool: {_boolValue}";
+        }
+
+        [Benchmark]
+        public void MixedInterpolation()
+        {
+            _ = $"String: {_stringValue}, Int: {_intValue}, DateTime: {_dateTimeValue}";
+        }
+
+        [Benchmark]
+        public void ComplexInterpolation()
+        {
+            _ = $"Values - Byte: {_byteValue}, Long: {_longValue}, Double: {_doubleValue}, Bool: {_boolValue}";
+        }
+    }
+}
